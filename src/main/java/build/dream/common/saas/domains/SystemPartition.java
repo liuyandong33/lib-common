0309ac9a569a2715fb77e5dcbcd846ea10e76f4b
liuyandong <554030404@qq.com>
2017-07-25 17:00:08 +0800

    初始化库！！！

---
+++ src/main/java/build/dream/common/saas/domains/SystemPartition.java
@@ -0,0 +1,106 @@
+package build.dream.common.saas.domains;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+public class SystemPartition {
+    private BigInteger id;
+    private String deploymentEnvironment;
+    private String partitionCode;
+    private String serviceName;
+    private String serviceDomain;
+    private String outsideServiceDomain;
+    private Date createTime;
+    private Integer createUserId;
+    private Date lastUpdateTime;
+    private Integer lastUpdateUserId;
+    private boolean deleted;
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    public String getDeploymentEnvironment() {
+        return deploymentEnvironment;
+    }
+
+    public void setDeploymentEnvironment(String deploymentEnvironment) {
+        this.deploymentEnvironment = deploymentEnvironment;
+    }
+
+    public String getPartitionCode() {
+        return partitionCode;
+    }
+
+    public void setPartitionCode(String partitionCode) {
+        this.partitionCode = partitionCode;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public String getServiceDomain() {
+        return serviceDomain;
+    }
+
+    public void setServiceDomain(String serviceDomain) {
+        this.serviceDomain = serviceDomain;
+    }
+
+    public String getOutsideServiceDomain() {
+        return outsideServiceDomain;
+    }
+
+    public void setOutsideServiceDomain(String outsideServiceDomain) {
+        this.outsideServiceDomain = outsideServiceDomain;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getCreateUserId() {
+        return createUserId;
+    }
+
+    public void setCreateUserId(Integer createUserId) {
+        this.createUserId = createUserId;
+    }
+
+    public Date getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(Date lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    public Integer getLastUpdateUserId() {
+        return lastUpdateUserId;
+    }
+
+    public void setLastUpdateUserId(Integer lastUpdateUserId) {
+        this.lastUpdateUserId = lastUpdateUserId;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+}
