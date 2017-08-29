286cf8e1a1c7badbbae49e0db1c240e872a4bdfb
liuyandong <554030404@qq.com>
2017-08-29 15:43:47 +0800

    初始化库！！！

--- src/main/java/build/dream/common/utils/SystemPartitionUtils.java
+++ src/main/java/build/dream/common/utils/SystemPartitionUtils.java
@@ -17,7 +17,7 @@ public class SystemPartitionUtils {
         String serviceDomain = PropertyUtils.getProperty(partitionCode + "." + serviceName + ".service.connection");
         if (StringUtils.isBlank(serviceDomain)) {
             String deploymentEnvironment = PropertyUtils.getProperty(Constants.DEPLOYMENT_ENVIRONMENT);
-            serviceDomain = CacheUtils.hget(Constants.KEY_SERVICE_DOMAIN, "_" + deploymentEnvironment + "_" + partitionCode + "_" + serviceName);
+            serviceDomain = CacheUtils.hget(Constants.KEY_SERVICE_DOMAIN + "_" + deploymentEnvironment, "_" + deploymentEnvironment + "_" + partitionCode + "_" + serviceName);
         }
         return serviceDomain;
     }
@@ -26,7 +26,7 @@ public class SystemPartitionUtils {
         String outsideServiceDomain = PropertyUtils.getProperty("outside." + partitionCode + "." + serviceName + ".service.connection");
         if (StringUtils.isBlank(outsideServiceDomain)) {
             String deploymentEnvironment = PropertyUtils.getProperty(Constants.KEY_OUTSIDE_SERVICE_DOMAIN);
-            outsideServiceDomain = CacheUtils.hget(Constants.KEY_OUTSIDE_SERVICE_DOMAIN, "_" + deploymentEnvironment + "_" + partitionCode + "_" + serviceName);
+            outsideServiceDomain = CacheUtils.hget(Constants.KEY_OUTSIDE_SERVICE_DOMAIN + "_" + deploymentEnvironment, "_" + deploymentEnvironment + "_" + partitionCode + "_" + serviceName);
         }
         return outsideServiceDomain;
     }
