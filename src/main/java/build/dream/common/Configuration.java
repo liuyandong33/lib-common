c358250e7e1f725973c0c88aa7fa74326a9e12d2
liuyandong <554030404@qq.com>
2017-08-31 18:57:10 +0800

    初始化库！！！

---
+++ src/main/java/build/dream/common/Configuration.java
@@ -0,0 +1,124 @@
+package build.dream.common;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+public class Configuration {
+    private BigInteger id;
+    private String deploymentEnvironment;
+    private String partitionCode;
+    private String serviceName;
+    private String configurationKey;
+    private String configurationValue;
+    private Integer type;
+    private String path;
+    private Date createTime;
+    private BigInteger createUserId;
+    private Date lastUpdateTime;
+    private BigInteger lastUpdateUserId;
+    private boolean deleted;
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    public String getDeploymentEnvironment() {
+        return deploymentEnvironment;
+    }
+
+    public void setDeploymentEnvironment(String deploymentEnvironment) {
+        this.deploymentEnvironment = deploymentEnvironment;
+    }
+
+    public String getPartitionCode() {
+        return partitionCode;
+    }
+
+    public void setPartitionCode(String partitionCode) {
+        this.partitionCode = partitionCode;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public String getConfigurationKey() {
+        return configurationKey;
+    }
+
+    public void setConfigurationKey(String configurationKey) {
+        this.configurationKey = configurationKey;
+    }
+
+    public String getConfigurationValue() {
+        return configurationValue;
+    }
+
+    public void setConfigurationValue(String configurationValue) {
+        this.configurationValue = configurationValue;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public BigInteger getCreateUserId() {
+        return createUserId;
+    }
+
+    public void setCreateUserId(BigInteger createUserId) {
+        this.createUserId = createUserId;
+    }
+
+    public Date getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(Date lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    public BigInteger getLastUpdateUserId() {
+        return lastUpdateUserId;
+    }
+
+    public void setLastUpdateUserId(BigInteger lastUpdateUserId) {
+        this.lastUpdateUserId = lastUpdateUserId;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+}
