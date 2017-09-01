d2ec7d1e5fde3cb44f56ac8dc0b4ae020957adb0
liuyandong <554030404@qq.com>
2017-09-01 17:03:49 +0800

    初始化库！！！

---
+++ catering-db.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS merge_user_branch;
+CREATE TABLE merge_user_branch
+(
+  id BIGINT NOT NULL PRIMARY KEY COMMENT 'ID',
+  user_id BIGINT NOT NULL COMMENT '用户ID',
+  tenant_id BIGINT NOT NULL COMMENT '商户ID',
+  branch_id BIGINT NOT NULL COMMENT '门店ID'
+);
+
+DROP TABLE IF EXISTS branch;
+CREATE TABLE branch
+(
+  id BIGINT NOT NULL PRIMARY KEY COMMENT 'ID',
+  `code` VARCHAR(20) NOT NULL COMMENT '门店编码',
+  `name` VARCHAR(20) NOT NULL COMMENT '门店名称',
+  `type` TINYINT NOT NULL COMMENT '1-总部，2-直营店，3-加盟店，4-配送中心',
+  `status` TINYINT NOT NULL COMMENT '状态，1-启用，2-停用',
+  create_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  create_user_id INT(11) NOT NULL COMMENT '创建人id',
+  last_update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+  last_update_user_id INT(11) NOT NULL COMMENT '最后更新人id',
+  deleted TINYINT(4) NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
+);
---
+++ saas-db.sql
@@ -0,0 +1,54 @@
+DROP TABLE IF EXISTS system_partition;
+CREATE TABLE system_partition (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
+    deployment_environment VARCHAR(20) NOT NULL COMMENT '部署环境',
+    partition_code VARCHAR(20) NOT NULL COMMENT '分区码',
+    `type` TINYINT NOT NULL COMMENT '服务类型，1-公共服务，2-分区服务',
+    service_name VARCHAR(20) NOT NULL COMMENT '服务名称',
+    service_domain VARCHAR(200) NOT NULL COMMENT '服务地址',
+    outside_service_domain VARCHAR(200) NOT NULL COMMENT '外网服务地址',
+    create_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
+    create_user_id BIGINT NOT NULL COMMENT '创建用户ID',
+    last_update_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
+    last_update_user_id BIGINT NOT NULL COMMENT '最后更新用户id',
+    deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
+);
+
+DROP TABLE IF EXISTS configuration;
+CREATE TABLE configuration (
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'id',
+    deployment_environment VARCHAR(20) NOT NULL COMMENT '部署环境',
+    partition_code VARCHAR(20) NOT NULL COMMENT '分区码',
+    service_name VARCHAR(20) NOT NULL COMMENT '服务名称',
+    configuration_key VARCHAR(200) COMMENT '配置key',
+    configuration_value VARCHAR(200) DEFAULT NULL COMMENT '配置value',
+    `type` TINYINT(4) DEFAULT 1 COMMENT '配置类型，1-application.properties，2-logback.properties，3.pro.properties',
+    path VARCHAR(200) DEFAULT NULL COMMENT '配置文件路径',
+    create_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    create_user_id INT(11) NOT NULL COMMENT '创建人id',
+    last_update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+    last_update_user_id INT(11) NOT NULL COMMENT '最后更新人id',
+    deleted TINYINT(4) NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
+);
+
+DROP TABLE IF EXISTS tenant;
+CREATE TABLE tenant
+(
+    id BIGINT NOT NULL PRIMARY KEY COMMENT 'ID',
+    `code` VARCHAR(20) NOT NULL COMMENT '商户编码',
+    `name` VARCHAR(20) NOT NULL COMMENT '商户名称',
+    modile VARCHAR(20) NOT NULL COMMENT '联系电话',
+    email VARCHAR(20) NOT NULL COMMENT '邮箱',
+    linkman VARCHAR(20) NOT NULL COMMENT '联系人',
+    business VARCHAR(10) NOT NULL COMMENT '业态',
+    province_code VARCHAR(10) NOT NULL COMMENT '省编码',
+    city_code VARCHAR(10) NOT NULL COMMENT '市编码',
+    district_code VARCHAR(10) NOT NULL COMMENT '区编码',
+    goods_table_name VARCHAR(20) NOT NULL COMMENT '产品表表名',
+    user_id BIGINT NOT NULL COMMENT '用户ID',
+    create_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    create_user_id INT(11) NOT NULL COMMENT '创建人id',
+    last_update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+    last_update_user_id INT(11) NOT NULL COMMENT '最后更新人id',
+    deleted TINYINT(4) NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
+);
