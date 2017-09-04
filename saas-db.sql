78a093503a11c5c55edfdd1a1a3c41d4f68fc5d4
liuyandong <554030404@qq.com>
2017-09-04 22:58:43 +0800

    初始化项目！！！

--- saas-db.sql
+++ saas-db.sql
@@ -150,11 +150,11 @@ CREATE TABLE order (
     payable_amount DECIMAL(11, 3) COMMENT '应付金额',
     paid_amount DECIMAL(11, 3) COMMENT '实付金额',
     paid_type TINYINT COMMENT '支付类型，1-微信支付，2-支付宝支付',
-    user_id BIGINT NOT NULL COMMENT '用户ID',
     create_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
     create_user_id BIGINT NOT NULL COMMENT '创建人id',
     last_update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE NOW() COMMENT '最后更新时间',
     last_update_user_id BIGINT NOT NULL COMMENT '最后更新人id',
+    last_update_remark VARCHAR(255) COMMENT '最后更新备注',
     deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
 );
 
@@ -168,11 +168,11 @@ CREATE TABLE order_detail (
     discount_amount DECIMAL(11, 3) NOT NULL COMMENT '优惠金额',
     real_price DECIMAL(11, 3) NOT NULL COMMENT '实际价格',
     amount INT NOT NULL COMMENT '数量',
-    user_id BIGINT NOT NULL COMMENT '用户ID',
     create_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
     create_user_id BIGINT NOT NULL COMMENT '创建人id',
     last_update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE NOW() COMMENT '最后更新时间',
     last_update_user_id BIGINT NOT NULL COMMENT '最后更新人id',
+    last_update_remark VARCHAR(255) COMMENT '最后更新备注',
     deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
 );
 
