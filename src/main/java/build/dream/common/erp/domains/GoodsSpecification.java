4fe4cc21ddecd1288bec0a46b102f2552f6f206f
liuyandong <554030404@qq.com>
2017-09-10 17:15:42 +0800

    饿了么对接！！！

---
+++ src/main/java/build/dream/common/erp/domains/GoodsSpecification.java
@@ -0,0 +1,125 @@
+package build.dream.common.erp.domains;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Date;
+
+public class GoodsSpecification {
+    private BigInteger id;
+    private BigInteger goodsId;
+    private String name;
+    private BigDecimal price;
+    private BigInteger tenantId;
+    private String tenantCode;
+    private BigInteger branchId;
+    private Date createTime;
+    private BigInteger createUserId;
+    private Date lastUpdateTime;
+    private BigInteger lastUpdateUserId;
+    private String lastUpdateRemark;
+    private boolean deleted;
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    public BigInteger getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(BigInteger goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigInteger getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(BigInteger tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public String getTenantCode() {
+        return tenantCode;
+    }
+
+    public void setTenantCode(String tenantCode) {
+        this.tenantCode = tenantCode;
+    }
+
+    public BigInteger getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(BigInteger branchId) {
+        this.branchId = branchId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public BigInteger getCreateUserId() {
+        return createUserId;
+    }
+
+    public void setCreateUserId(BigInteger createUserId) {
+        this.createUserId = createUserId;
+    }
+
+    public Date getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(Date lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    public BigInteger getLastUpdateUserId() {
+        return lastUpdateUserId;
+    }
+
+    public void setLastUpdateUserId(BigInteger lastUpdateUserId) {
+        this.lastUpdateUserId = lastUpdateUserId;
+    }
+
+    public String getLastUpdateRemark() {
+        return lastUpdateRemark;
+    }
+
+    public void setLastUpdateRemark(String lastUpdateRemark) {
+        this.lastUpdateRemark = lastUpdateRemark;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+}
