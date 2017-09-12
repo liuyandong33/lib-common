5606125d61b8f300e5a0a1979be91b66da8ce730
liuyandong <554030404@qq.com>
2017-09-12 17:15:51 +0800

    初始化库！！！

---
+++ src/main/java/build/dream/common/saas/domains/AlipayOpenAuthToken.java
@@ -0,0 +1,133 @@
+package build.dream.common.saas.domains;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+public class AlipayOpenAuthToken {
+    private BigInteger id;
+    private String appId;
+    private String appAuthToken;
+    private String userId;
+    private String authAppId;
+    private Integer expiresIn;
+    private Integer reExpiresIn;
+    private String appRefreshToken;
+    private Date createTime;
+    private BigInteger createUserId;
+    private Date lastUpdateTime;
+    private BigInteger lastUpdateUserId;
+    private String lastUpdateRemark;
+    private boolean deleted;
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public String getAppAuthToken() {
+        return appAuthToken;
+    }
+
+    public void setAppAuthToken(String appAuthToken) {
+        this.appAuthToken = appAuthToken;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getAuthAppId() {
+        return authAppId;
+    }
+
+    public void setAuthAppId(String authAppId) {
+        this.authAppId = authAppId;
+    }
+
+    public Integer getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(Integer expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+
+    public Integer getReExpiresIn() {
+        return reExpiresIn;
+    }
+
+    public void setReExpiresIn(Integer reExpiresIn) {
+        this.reExpiresIn = reExpiresIn;
+    }
+
+    public String getAppRefreshToken() {
+        return appRefreshToken;
+    }
+
+    public void setAppRefreshToken(String appRefreshToken) {
+        this.appRefreshToken = appRefreshToken;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public BigInteger getCreateUserId() {
+        return createUserId;
+    }
+
+    public void setCreateUserId(BigInteger createUserId) {
+        this.createUserId = createUserId;
+    }
+
+    public Date getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(Date lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    public BigInteger getLastUpdateUserId() {
+        return lastUpdateUserId;
+    }
+
+    public void setLastUpdateUserId(BigInteger lastUpdateUserId) {
+        this.lastUpdateUserId = lastUpdateUserId;
+    }
+
+    public String getLastUpdateRemark() {
+        return lastUpdateRemark;
+    }
+
+    public void setLastUpdateRemark(String lastUpdateRemark) {
+        this.lastUpdateRemark = lastUpdateRemark;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+}
