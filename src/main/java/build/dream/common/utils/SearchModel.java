1deb1bf0d3dfc06c482c7250acf4b0d4c86ade6f
liuyandong <554030404@qq.com>
2017-09-21 09:52:35 +0800

    初始化库！！！

--- src/main/java/build/dream/common/utils/SearchModel.java
+++ src/main/java/build/dream/common/utils/SearchModel.java
@@ -1,19 +1,54 @@
 package build.dream.common.utils;
 
+import build.dream.common.constants.Constants;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class SearchModel {
-    public List<SearchCondition> searchConditions = new ArrayList<SearchCondition>();
+    private List<SearchCondition> searchConditions = new ArrayList<SearchCondition>();
+    private List<String> columns = new ArrayList<String>();
 
     public SearchModel() {
 
     }
 
-    public SearchModel(boolean setDeletedSearchCondition) {
-        if (setDeletedSearchCondition) {
-            this.addSearchCondition("deleted", "=", 0);
+    public SearchModel(boolean autoSetDeletedFalse) {
+        if (autoSetDeletedFalse) {
+            this.addSearchCondition("deleted", Constants.SQL_OPERATION_SYMBOL_EQUALS, 0);
         }
+    }
+
+    public List<SearchCondition> getSearchConditions() {
+        return searchConditions;
+    }
+
+    public void setSearchConditions(List<SearchCondition> searchConditions) {
+        this.searchConditions = searchConditions;
+    }
+
+    public String getSelectColumns() {
+        String selectColumns = null;
+        if (CollectionUtils.isEmpty(columns)) {
+            selectColumns = "*";
+        } else {
+            selectColumns = StringUtils.join(columns, ", ");
+        }
+        return selectColumns;
+    }
+
+    public List<String> getColumns() {
+        return columns;
+    }
+
+    public void setColumns(List<String> columns) {
+        this.columns = columns;
+    }
+
+    public void addColumns(String... elements) {
+        CollectionUtils.addAll(this.columns, elements);
     }
 
     public void addSearchCondition(String columnName, String operationSymbol, Object searchParameter) {
