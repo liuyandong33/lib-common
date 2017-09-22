b9d11e19c652a619968c5bbca0da2e41a58f8e23
liuyandong <554030404@qq.com>
2017-09-22 13:40:08 +0800

    初始化库！！！

--- src/main/java/build/dream/common/utils/GsonUtils.java
+++ src/main/java/build/dream/common/utils/GsonUtils.java
@@ -3,6 +3,7 @@ package build.dream.common.utils;
 import build.dream.common.constants.Constants;
 import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
+import net.sf.json.JSONObject;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -12,17 +13,18 @@ import java.util.List;
  * Created by liuyandong on 2017/3/23.
  */
 public class GsonUtils {
-    private static Gson instantiateGson(String datePattern) {
-        GsonBuilder gsonBuilder = new GsonBuilder().setDateFormat(datePattern).serializeNulls().disableHtmlEscaping();
+    private static Gson instantiateGson(String datePattern, boolean serializeNulls) {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setDateFormat(datePattern);
+        gsonBuilder.disableHtmlEscaping();
+        if (serializeNulls) {
+            gsonBuilder.serializeNulls();
+        }
         return gsonBuilder.create();
     }
 
-    public static Gson instantiateGson() {
-        return instantiateGson(Constants.DEFAULT_DATE_PATTERN);
-    }
-
     public static <T> T jsonToObject(String jsonString, Class<T> clazz, String datePattern) {
-        return instantiateGson(datePattern).fromJson(jsonString, clazz);
+        return instantiateGson(datePattern, true).fromJson(jsonString, clazz);
     }
 
     public static <T> T jsonToObject(String jsonString, Class<T> clazz) {
@@ -30,7 +32,7 @@ public class GsonUtils {
     }
 
     public static <T> List<T> jsonToList(String jsonString, Class<T> clazz, String datePattern) {
-        Gson gson = instantiateGson(datePattern);
+        Gson gson = instantiateGson(datePattern, true);
         Type type = new TypeToken<ArrayList<JsonObject>>() {
         }.getType();
         List<JsonObject> jsonObjects = gson.fromJson(jsonString, type);
@@ -45,20 +47,44 @@ public class GsonUtils {
         return jsonToList(jsonString, clazz, Constants.DEFAULT_DATE_PATTERN);
     }
 
-    public static String toJson(Object object, String datePattern) {
-        return instantiateGson(datePattern).toJson(object);
+    public static String toJson(Object object) {
+        return instantiateGson(Constants.DEFAULT_DATE_PATTERN, true).toJson(object);
     }
 
-    public static String toJson(Object object) {
-        return toJson(object, Constants.DEFAULT_DATE_PATTERN);
+    public static String toJson(Object object, String datePattern) {
+        return instantiateGson(datePattern, true).toJson(object);
+    }
+
+    public static String toJson(Object object, boolean serializeNulls) {
+        return instantiateGson(Constants.DEFAULT_DATE_PATTERN, serializeNulls).toJson(object);
+    }
+
+    public static String toJson(Object object, String datePattern, boolean serializeNulls) {
+        return instantiateGson(datePattern, serializeNulls).toJson(object);
     }
 
     public static <T> T fromJson(String jsonString, Class<T> clazz) {
-        return instantiateGson().fromJson(jsonString, clazz);
+        return instantiateGson(Constants.DEFAULT_DATE_PATTERN, true).fromJson(jsonString, clazz);
     }
 
     public static <T> T fromJson(String jsonString, Class<T> clazz, String datePattern) {
-        return instantiateGson(datePattern).fromJson(jsonString, clazz);
+        return instantiateGson(datePattern, true).fromJson(jsonString, clazz);
+    }
+
+    public static <T> T fromJson(JsonElement jsonElement, Class<T> clazz) {
+        return instantiateGson(Constants.DEFAULT_DATE_PATTERN, true).fromJson(jsonElement, clazz);
+    }
+
+    public static <T> T fromJson(JsonElement jsonElement, Class<T> clazz, String datePattern) {
+        return instantiateGson(datePattern, true).fromJson(jsonElement, clazz);
+    }
+
+    public static <T> T fromJson(JSONObject jsonObject, Class<T> clazz) {
+        return instantiateGson(Constants.DEFAULT_DATE_PATTERN, true).fromJson(jsonObject.toString(), clazz);
+    }
+
+    public static <T> T fromJson(JSONObject jsonObject, Class<T> clazz, String datePattern) {
+        return instantiateGson(datePattern, true).fromJson(jsonObject.toString(), clazz);
     }
 
     public static JsonObject parseJsonObject(String jsonString) {
