ab0c278cd7a55cb7ae58b50b7e788c49dc84a843
liuyandong <554030404@qq.com>
2017-09-25 17:50:19 +0800

    微信开发！！

---
+++ src/main/java/build/dream/common/beans/WeiXinOAuthAccessToken.java
@@ -0,0 +1,60 @@
+package build.dream.common.beans;
+
+import java.util.Date;
+
+public class WeiXinOAuthAccessToken {
+    private String accessToken;
+    private Integer expiresIn;
+    private String refreshToken;
+    private String openid;
+    private String scope;
+    private Date fetchTime;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public Integer getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(Integer expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public String getOpenid() {
+        return openid;
+    }
+
+    public void setOpenid(String openid) {
+        this.openid = openid;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public Date getFetchTime() {
+        return fetchTime;
+    }
+
+    public void setFetchTime(Date fetchTime) {
+        this.fetchTime = fetchTime;
+    }
+}
