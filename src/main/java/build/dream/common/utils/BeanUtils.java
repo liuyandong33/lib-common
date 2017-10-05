c4fb86e46bc736795e0b01977b9fc05f26fe2be2
liuyandong <554030404@qq.com>
2017-10-05 21:55:22 +0800

    错误修改！！

--- src/main/java/build/dream/common/utils/BeanUtils.java
+++ src/main/java/build/dream/common/utils/BeanUtils.java
@@ -1,10 +1,13 @@
 package build.dream.common.utils;
 
+import build.dream.common.saas.domains.Tenant;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.util.ReflectionUtils;
 
 import java.lang.reflect.Field;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class BeanUtils {
     public static Map<String, Object> beanToMap(Object object) {
@@ -30,5 +33,53 @@ public class BeanUtils {
             }
         }
         return t;
+    }
+
+    public static String beanToXml(Object object, String rootNodeName) throws IllegalAccessException {
+        Class<?> objectClass = object.getClass();
+        if (StringUtils.isBlank(rootNodeName)) {
+            rootNodeName = "xml";
+        }
+        StringBuffer xml = new StringBuffer("<");
+        xml.append(rootNodeName);
+        xml.append(">");
+        Field[] fields = objectClass.getDeclaredFields();
+        for (Field field : fields) {
+            ReflectionUtils.makeAccessible(field);
+            String fieldName = field.getName();
+            Object fieldValue = field.get(object);
+            if (fieldValue != null) {
+                xml.append("<").append(fieldName).append(">").append("<![CDATA[");
+                xml.append(GsonUtils.toJson(fieldValue)).append("]]>").append("</").append(fieldName).append(">");
+            }
+        }
+        xml.append("</").append(rootNodeName);
+        return xml.toString();
+    }
+
+    public static String mapToXml(Map<String, Object> map, String rootNodeName) {
+        if (StringUtils.isBlank(rootNodeName)) {
+            rootNodeName = "xml";
+        }
+        StringBuffer xml = new StringBuffer("<");
+        xml.append(rootNodeName);
+        xml.append(">");
+        Set<Map.Entry<String, Object>> entries = map.entrySet();
+        for (Map.Entry<String, Object> entry : entries) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            if (value != null) {
+                xml.append("<").append(key).append(">").append("<![CDATA[");
+                xml.append(GsonUtils.toJson(value)).append("]]>").append("</").append(key).append(">");
+            }
+        }
+        xml.append("</").append(rootNodeName);
+        return xml.toString();
+    }
+
+    public static void main(String[] args) throws IllegalAccessException {
+        Tenant tenant = new Tenant();
+        Map<String, Object> map = beanToMap(tenant);
+        System.out.println(mapToXml(map, null));
     }
 }
