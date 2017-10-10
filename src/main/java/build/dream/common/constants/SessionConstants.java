5c0958c615b82fc53e8b01f9f34d00e00006c34e
liuyandong <554030404@qq.com>
2017-10-10 20:25:11 +0800

    错误修改！！

---
+++ src/main/java/build/dream/common/constants/SessionConstants.java
@@ -0,0 +1,16 @@
+package build.dream.common.constants;
+
+/**
+ * Created by liuyandong on 2017/7/24.
+ */
+public class SessionConstants {
+    public static final String KEY_USER_TYPE = "_user_type";
+    public static final String KEY_USER_ID = "_user_id";
+    public static final String KEY_USER_NAME = "_user_name";
+    public static final String KEY_USER_MOBILE = "_user_mobile";
+    public static final String KEY_USER_EMAIL = "_user_email";
+    public static final String KEY_TENANT_ID = "_tenant_id";
+    public static final String KEY_TENANT_CODE = "_tenant_code";
+    public static final String KEY_TENANT_NAME = "_tenant_name";
+    public static final String KEY_TENANT_PARTITION_CODE = "_tenant_partition_code";
+}
