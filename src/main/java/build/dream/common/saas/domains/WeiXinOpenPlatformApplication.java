33388885936c9b93db49117b7be48d35577fea0f
liuyandong <554030404@qq.com>
2017-10-27 23:54:33 +0800

    错误修改！！

---
+++ src/main/java/build/dream/common/saas/domains/WeiXinOpenPlatformApplication.java
@@ -0,0 +1,24 @@
+package build.dream.common.saas.domains;
+
+import build.dream.common.basic.BasicDomain;
+
+public class WeiXinOpenPlatformApplication extends BasicDomain {
+    private String appId;
+    private String appSecret;
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public String getAppSecret() {
+        return appSecret;
+    }
+
+    public void setAppSecret(String appSecret) {
+        this.appSecret = appSecret;
+    }
+}
