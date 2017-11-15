53bc1f841b0395438b1106f1f3293704f566ce63
liuyandong <554030404@qq.com>
2017-11-15 12:46:47 +0800

    微信开发！！

---
+++ src/main/java/build/dream/common/saas/domains/ElemeCallbackMessage.java
@@ -0,0 +1,80 @@
+package build.dream.common.saas.domains;
+
+import build.dream.common.basic.BasicDomain;
+
+import java.math.BigInteger;
+
+public class ElemeCallbackMessage extends BasicDomain {
+    private String requestId;
+    private Integer type;
+    private BigInteger appId;
+    private String message;
+    private BigInteger shopId;
+    private BigInteger timestamp;
+    private String signature;
+    private BigInteger userId;
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public BigInteger getAppId() {
+        return appId;
+    }
+
+    public void setAppId(BigInteger appId) {
+        this.appId = appId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public BigInteger getShopId() {
+        return shopId;
+    }
+
+    public void setShopId(BigInteger shopId) {
+        this.shopId = shopId;
+    }
+
+    public BigInteger getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(BigInteger timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+
+    public BigInteger getUserId() {
+        return userId;
+    }
+
+    public void setUserId(BigInteger userId) {
+        this.userId = userId;
+    }
+}
