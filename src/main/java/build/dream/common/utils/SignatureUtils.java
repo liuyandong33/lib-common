63745c50e69cbab29d166db25c1e0b226971bdde
liuyandong <554030404@qq.com>
2017-12-04 09:35:14 +0800

    签名工具开发！！！

--- src/main/java/build/dream/common/utils/SignatureUtils.java
+++ src/main/java/build/dream/common/utils/SignatureUtils.java
@@ -28,21 +28,21 @@ public class SignatureUtils {
     }
 
     public static String sign(String data, String privateKey) throws NoSuchAlgorithmException, SignatureException, InvalidKeySpecException, InvalidKeyException, UnsupportedEncodingException {
-        return sign(data, privateKey, SIGNATURE_ALGORITHM_SHA1_WITH_RSA, OUTPUT_TYPE_BASE64);
+        return sign(data, Constants.CHARSET_NAME_UTF_8, privateKey, SIGNATURE_ALGORITHM_SHA1_WITH_RSA, OUTPUT_TYPE_BASE64);
     }
 
-    public static String sign(String data, String privateKey, String signatureType, String outputType) throws InvalidKeySpecException, NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException, SignatureException {
-        return sign(data, RSAUtils.restorePrivateKey(privateKey), signatureType, outputType);
+    public static String sign(String data, String charsetName, String privateKey, String signatureType, String outputType) throws InvalidKeySpecException, NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException, SignatureException {
+        return sign(data, charsetName, RSAUtils.restorePrivateKey(privateKey), signatureType, outputType);
     }
 
     public static String sign(String data, PrivateKey privateKey) throws SignatureException, NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException {
-        return sign(data, privateKey, SIGNATURE_ALGORITHM_SHA1_WITH_RSA, OUTPUT_TYPE_BASE64);
+        return sign(data,Constants.CHARSET_NAME_UTF_8, privateKey, SIGNATURE_ALGORITHM_SHA1_WITH_RSA, OUTPUT_TYPE_BASE64);
     }
 
-    public static String sign(String data, PrivateKey privateKey, String signatureType, String outputType) throws NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException, SignatureException {
+    public static String sign(String data, String charsetName, PrivateKey privateKey, String signatureType, String outputType) throws NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException, SignatureException {
         Signature signature = obtainSignature(signatureType);
         signature.initSign(privateKey);
-        signature.update(data.getBytes(Constants.CHARSET_UTF_8));
+        signature.update(data.getBytes(charsetName));
         if (OUTPUT_TYPE_BASE64.equals(outputType)) {
             return Base64.encodeBase64String(signature.sign());
         } else if (OUTPUT_TYPE_HEX.equals(outputType)) {
@@ -52,22 +52,22 @@ public class SignatureUtils {
         }
     }
 
-    public static boolean verifySign(String data, String publicKey, String signed) throws NoSuchAlgorithmException, SignatureException, InvalidKeySpecException, InvalidKeyException, DecoderException, UnsupportedEncodingException {
-        return verifySign(data, publicKey, SIGNATURE_TYPE_SHA1_WITH_RSA, OUTPUT_TYPE_BASE64, signed);
+    public static boolean verifySign(String data, String publicKey, String sign) throws NoSuchAlgorithmException, SignatureException, InvalidKeySpecException, InvalidKeyException, DecoderException, UnsupportedEncodingException {
+        return verifySign(data, Constants.CHARSET_NAME_UTF_8, publicKey, SIGNATURE_TYPE_SHA1_WITH_RSA, OUTPUT_TYPE_BASE64, sign);
     }
 
-    public static boolean verifySign(String data, String publicKey, String signatureType, String outputType, String signed) throws InvalidKeySpecException, NoSuchAlgorithmException, DecoderException, InvalidKeyException, SignatureException, UnsupportedEncodingException {
-        return verifySign(data, RSAUtils.restorePublicKey(publicKey), signatureType, outputType, signed);
+    public static boolean verifySign(String data, String charsetName, String publicKey, String signatureType, String outputType, String sign) throws InvalidKeySpecException, NoSuchAlgorithmException, DecoderException, InvalidKeyException, SignatureException, UnsupportedEncodingException {
+        return verifySign(data, charsetName, RSAUtils.restorePublicKey(publicKey), signatureType, outputType, sign);
     }
 
-    public static boolean verifySign(String data, PublicKey publicKey, String signed) throws DecoderException, NoSuchAlgorithmException, InvalidKeyException, SignatureException, UnsupportedEncodingException {
-        return verifySign(data, publicKey, SIGNATURE_TYPE_SHA1_WITH_RSA, OUTPUT_TYPE_BASE64, signed);
+    public static boolean verifySign(String data, PublicKey publicKey, String sign) throws DecoderException, NoSuchAlgorithmException, InvalidKeyException, SignatureException, UnsupportedEncodingException {
+        return verifySign(data, Constants.CHARSET_NAME_UTF_8, publicKey, SIGNATURE_TYPE_SHA1_WITH_RSA, OUTPUT_TYPE_BASE64, sign);
     }
 
-    public static boolean verifySign(String data, PublicKey publicKey, String signatureType, String outputType, String signed) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, DecoderException, UnsupportedEncodingException {
+    public static boolean verifySign(String data, String charsetName, PublicKey publicKey, String signatureType, String outputType, String signed) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, DecoderException, UnsupportedEncodingException {
         Signature signature = obtainSignature(signatureType);
         signature.initVerify(publicKey);
-        signature.update(data.getBytes(Constants.CHARSET_UTF_8));
+        signature.update(data.getBytes(charsetName));
         if (OUTPUT_TYPE_BASE64.equals(outputType)) {
             return signature.verify(Base64.decodeBase64(signed));
         } else if (OUTPUT_TYPE_HEX.equals(outputType)) {
