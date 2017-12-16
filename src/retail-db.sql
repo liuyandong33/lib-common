8a6ad827a0698d578fccf70acacda68051a5dae0
liuyandong <554030404@qq.com>
2017-12-16 13:13:00 +0800

    微信开发！！

---
+++ src/retail-db.sql
@@ -0,0 +1,57 @@
+DROP TABLE IF EXISTS merge_user_branch;
+CREATE TABLE merge_user_branch
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
+    user_id BIGINT NOT NULL COMMENT '用户ID',
+    tenant_id BIGINT NOT NULL COMMENT '商户ID',
+    branch_id BIGINT NOT NULL COMMENT '门店ID',
+    deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
+);
+
+DROP TABLE IF EXISTS branch;
+CREATE TABLE branch
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
+    `code` VARCHAR(20) NOT NULL COMMENT '门店编码',
+    `name` VARCHAR(20) NOT NULL COMMENT '门店名称',
+    `type` TINYINT NOT NULL COMMENT '1-总部，2-直营店，3-加盟店，4-配送中心',
+    `status` TINYINT NOT NULL COMMENT '状态，1-启用，2-停用',
+    tenant_id BIGINT NOT NULL COMMENT '商户ID',
+    tenant_code VARCHAR(20) NOT NULL COMMENT '商户编码',
+    eleme_account_type TINYINT NOT NULL COMMENT '1-连锁账号，2-独立账号',
+    shop_id BIGINT COMMENT '饿了么店铺ID',
+    create_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
+    create_user_id BIGINT NOT NULL COMMENT '创建人id',
+    last_update_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
+    last_update_user_id BIGINT NOT NULL COMMENT '最后更新人id',
+    last_update_remark VARCHAR(255) COMMENT '最后更新备注',
+    deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
+);
+
+DROP TABLE IF EXISTS sequence;
+CREATE TABLE sequence
+(
+    name VARCHAR(50) PRIMARY KEY NOT NULL COMMENT '序列名称',
+    current_value INT(11) unsigned NOT NULL COMMENT '当前值',
+    increment INT(11) unsigned DEFAULT '1' NOT NULL COMMENT '每次增长的值'
+);
+
+DROP FUNCTION IF EXISTS current_value;
+CREATE FUNCTION current_value(seq_name VARCHAR(50)) RETURNS INT(11)
+BEGIN
+    DECLARE value int;
+    SET value = 0;
+    SELECT current_value INTO value FROM sequence WHERE name = seq_name;
+    IF value = 0 THEN
+        SET value = 1;
+        INSERT INTO sequence(name, current_value) VALUES(seq_name, value);
+    END if;
+    return value;
+END;
+
+DROP FUNCTION IF EXISTS next_value;
+CREATE FUNCTION next_value(seq_name VARCHAR(50)) RETURNS INT(11)
+BEGIN
+    UPDATE sequence SET current_value = current_value + increment where name = seq_name;
+    return current_value(seq_name);
+END ;
