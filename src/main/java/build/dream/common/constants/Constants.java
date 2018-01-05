8609843cea165b1a26020b6bdaf872bcaa94c710
liuyandong <554030404@qq.com>
2018-01-05 21:57:08 +0800

    增加验证方法！！

--- src/main/java/build/dream/common/constants/Constants.java
+++ src/main/java/build/dream/common/constants/Constants.java
@@ -146,4 +146,5 @@ public class Constants {
     public static final String KEY_PLATFORM_PRIVATE_KEY = "_platform_private_key";
 
     public static final String NETWORK_ERROR_MESSAGE = "网络错误！";
+    public static final String KEY_TENANT_SECRET_KEYS = "_tenant_secret_keys";
 }
