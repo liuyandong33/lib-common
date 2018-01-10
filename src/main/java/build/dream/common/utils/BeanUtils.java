b6916a765cdad0526fd3c565c0dcf218af985623
liuyandong <554030404@qq.com>
2018-01-10 16:07:50 +0800

    微信开发！！

--- src/main/java/build/dream/common/utils/BeanUtils.java
+++ src/main/java/build/dream/common/utils/BeanUtils.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.util.ReflectionUtils;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
@@ -75,5 +76,81 @@ public class BeanUtils {
         }
         xml.append("</").append(rootNodeName);
         return xml.toString();
+    }
+
+    public static String generateInsertSql(String className) {
+        Class clazz = null;
+        try {
+            clazz = Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        StringBuilder insertSql = new StringBuilder("INSERT INTO ");
+        String classSimpleName = clazz.getSimpleName();
+        insertSql.append(NamingStrategyUtils.camelCaseToUnderscore(classSimpleName.substring(0, 1).toLowerCase() + classSimpleName.substring(1)));
+        insertSql.append("(");
+        StringBuilder valuesSql = new StringBuilder(" VALUES(");
+        while (clazz != Object.class) {
+            Field[] fields = clazz.getDeclaredFields();
+            for (Field field : fields) {
+                int modifiers = field.getModifiers();
+                if (Modifier.isStatic(modifiers) || Modifier.isFinal(modifiers) || Modifier.isNative(modifiers)) {
+                    continue;
+                }
+                String fieldName = field.getName();
+                if ("id".equals(fieldName) || "createTime".equals(fieldName) || "lastUpdateTime".equals(fieldName) || "deleted".equals(fieldName)) {
+                    continue;
+                }
+                insertSql.append(NamingStrategyUtils.camelCaseToUnderscore(fieldName));
+                insertSql.append(", ");
+                valuesSql.append("#{").append(fieldName);
+                valuesSql.append("}, ");
+            }
+            clazz = clazz.getSuperclass();
+        }
+        insertSql.deleteCharAt(insertSql.length() - 1);
+        insertSql.deleteCharAt(insertSql.length() - 1);
+        insertSql.append(")");
+        valuesSql.deleteCharAt(valuesSql.length() - 1);
+        valuesSql.deleteCharAt(valuesSql.length() - 1);
+        valuesSql.append(")");
+        insertSql.append(valuesSql);
+        return insertSql.toString();
+    }
+
+    public static String generateUpdateSql(String className) {
+        Class clazz = null;
+        try {
+            clazz = Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        StringBuilder updateSql = new StringBuilder("UPDATE ");
+        String classSimpleName = clazz.getSimpleName();
+        updateSql.append(NamingStrategyUtils.camelCaseToUnderscore(classSimpleName.substring(0, 1).toLowerCase() + classSimpleName.substring(1)));
+        updateSql.append(" SET ");
+        while (clazz != Object.class) {
+            Field[] fields = clazz.getDeclaredFields();
+            for (Field field : fields) {
+                int modifiers = field.getModifiers();
+                if (Modifier.isStatic(modifiers) || Modifier.isFinal(modifiers) || Modifier.isNative(modifiers)) {
+                    continue;
+                }
+                String fieldName = field.getName();
+                if ("createTime".equals(fieldName) || "lastUpdateTime".equals(fieldName)) {
+                    continue;
+                }
+                updateSql.append(NamingStrategyUtils.camelCaseToUnderscore(fieldName));
+                updateSql.append(" = ");
+                updateSql.append("#{");
+                updateSql.append(fieldName);
+                updateSql.append("}, ");
+            }
+            clazz = clazz.getSuperclass();
+        }
+        updateSql.deleteCharAt(updateSql.length() - 1);
+        updateSql.deleteCharAt(updateSql.length() - 1);
+        updateSql.append(" WHERE id = #{id}");
+        return updateSql.toString();
     }
 }
