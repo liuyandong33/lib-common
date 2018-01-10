56fa84f2dcd88446c4075f32291b18018ae41bf7
liuyandong <554030404@qq.com>
2018-01-10 10:36:03 +0800

    微信开发！！

---
+++ src/main/java/build/dream/common/saas/domains/AppPrivilege.java
@@ -0,0 +1,80 @@
+package build.dream.common.saas.domains;
+
+import build.dream.common.basic.BasicDomain;
+
+import java.math.BigInteger;
+
+public class AppPrivilege extends BasicDomain {
+    private String privilegeCode;
+    private String serviceName;
+    private String resourceName;
+    private String controllerName;
+    private String operateName;
+    private String actionName;
+    private BigInteger parentId;
+    private String remark;
+
+    public String getPrivilegeCode() {
+        return privilegeCode;
+    }
+
+    public void setPrivilegeCode(String privilegeCode) {
+        this.privilegeCode = privilegeCode;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public void setResourceName(String resourceName) {
+        this.resourceName = resourceName;
+    }
+
+    public String getControllerName() {
+        return controllerName;
+    }
+
+    public void setControllerName(String controllerName) {
+        this.controllerName = controllerName;
+    }
+
+    public String getOperateName() {
+        return operateName;
+    }
+
+    public void setOperateName(String operateName) {
+        this.operateName = operateName;
+    }
+
+    public String getActionName() {
+        return actionName;
+    }
+
+    public void setActionName(String actionName) {
+        this.actionName = actionName;
+    }
+
+    public BigInteger getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(BigInteger parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
