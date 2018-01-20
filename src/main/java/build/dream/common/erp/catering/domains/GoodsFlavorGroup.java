c303a401e702606ad77bea6eac21fd225185ca0c
liuyandong <554030404@qq.com>
2018-01-20 18:27:32 +0800

    增加验证方法！！

--- src/main/java/build/dream/common/erp/catering/domains/GoodsFlavorGroup.java
+++ src/main/java/build/dream/common/erp/catering/domains/GoodsFlavorGroup.java
@@ -3,14 +3,10 @@ package build.dream.common.erp.catering.domains;
 import build.dream.common.basic.BasicDomain;
 
 import java.math.BigInteger;
-import java.util.Date;
 
 public class GoodsFlavorGroup extends BasicDomain {
     private BigInteger goodsId;
     private String name;
-    private BigInteger tenantId;
-    private String tenantCode;
-    private BigInteger branchId;
 
     public BigInteger getGoodsId() {
         return goodsId;
@@ -26,29 +22,5 @@ public class GoodsFlavorGroup extends BasicDomain {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public BigInteger getTenantId() {
-        return tenantId;
-    }
-
-    public void setTenantId(BigInteger tenantId) {
-        this.tenantId = tenantId;
-    }
-
-    public String getTenantCode() {
-        return tenantCode;
-    }
-
-    public void setTenantCode(String tenantCode) {
-        this.tenantCode = tenantCode;
-    }
-
-    public BigInteger getBranchId() {
-        return branchId;
-    }
-
-    public void setBranchId(BigInteger branchId) {
-        this.branchId = branchId;
     }
 }
--- src/main/java/build/dream/common/erp/catering/domains/GoodsSpecification.java
+++ src/main/java/build/dream/common/erp/catering/domains/GoodsSpecification.java
@@ -4,15 +4,11 @@ import build.dream.common.basic.BasicDomain;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.Date;
 
 public class GoodsSpecification extends BasicDomain {
     private BigInteger goodsId;
     private String name;
     private BigDecimal price;
-    private BigInteger tenantId;
-    private String tenantCode;
-    private BigInteger branchId;
 
     public BigInteger getGoodsId() {
         return goodsId;
@@ -36,29 +32,5 @@ public class GoodsSpecification extends BasicDomain {
 
     public void setPrice(BigDecimal price) {
         this.price = price;
-    }
-
-    public BigInteger getTenantId() {
-        return tenantId;
-    }
-
-    public void setTenantId(BigInteger tenantId) {
-        this.tenantId = tenantId;
-    }
-
-    public String getTenantCode() {
-        return tenantCode;
-    }
-
-    public void setTenantCode(String tenantCode) {
-        this.tenantCode = tenantCode;
-    }
-
-    public BigInteger getBranchId() {
-        return branchId;
-    }
-
-    public void setBranchId(BigInteger branchId) {
-        this.branchId = branchId;
     }
 }
