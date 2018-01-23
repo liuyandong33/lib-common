0bf1596f2c2b5abd6a4e6a467ee5522618cee047
liuyandong <554030404@qq.com>
2018-01-23 13:09:54 +0800

    微信开发！！

--- src/main/java/build/dream/common/Application.java
+++ src/main/java/build/dream/common/Application.java
@@ -1,9 +1,15 @@
 package build.dream.common;
 
-import org.springframework.boot.SpringApplication;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.ValidationMessage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.io.IOException;
+import java.util.Set;
 
 /**
  * Created by liuyandong on 2017/7/25.
@@ -15,7 +21,11 @@ public class Application extends SpringBootServletInitializer {
         return builder.sources(Application.class);
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    public static void main(String[] args) throws IOException {
+//        SpringApplication.run(Application.class, args);
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonSchema jsonSchema = JsonSchemaFactory.getInstance().getSchema("{\"type\":\"object\",\"properties\": {\"name\": {\"type\":\"string\",\"minLength\": 2,\"maxLength\": 3}},\"required\":[\"name\"]}");
+        Set<ValidationMessage> aa =  jsonSchema.validate(objectMapper.readTree("{\"name\": \"啊\"}"), objectMapper.readTree("{\"name\": \"啊\"}"), "aaa");
+        int a = 100;
     }
 }
