89835d6cbc6bc4c518f46bf1ad260296b6eeddf2
liuyandong <554030404@qq.com>
2018-01-26 17:59:03 +0800

    微信开发！！

---
+++ src/main/java/build/dream/common/erp/catering/domains/Vip.java
@@ -0,0 +1,99 @@
+package build.dream.common.erp.catering.domains;
+
+import build.dream.common.basic.BasicDomain;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+public class Vip extends BasicDomain {
+    private BigInteger tenantId;
+    private String tenantCode;
+    private BigInteger branchId;
+    private String vipCode;
+    private String vipName;
+    private Date birthday;
+    private String phoneNumber;
+    private String openId;
+    private String mainOpenId;
+    private String alipayUserId;
+
+    public BigInteger getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(BigInteger tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public String getTenantCode() {
+        return tenantCode;
+    }
+
+    public void setTenantCode(String tenantCode) {
+        this.tenantCode = tenantCode;
+    }
+
+    public BigInteger getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(BigInteger branchId) {
+        this.branchId = branchId;
+    }
+
+    public String getVipCode() {
+        return vipCode;
+    }
+
+    public void setVipCode(String vipCode) {
+        this.vipCode = vipCode;
+    }
+
+    public String getVipName() {
+        return vipName;
+    }
+
+    public void setVipName(String vipName) {
+        this.vipName = vipName;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId;
+    }
+
+    public String getMainOpenId() {
+        return mainOpenId;
+    }
+
+    public void setMainOpenId(String mainOpenId) {
+        this.mainOpenId = mainOpenId;
+    }
+
+    public String getAlipayUserId() {
+        return alipayUserId;
+    }
+
+    public void setAlipayUserId(String alipayUserId) {
+        this.alipayUserId = alipayUserId;
+    }
+}
