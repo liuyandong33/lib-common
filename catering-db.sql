87cd20d2cde4cc3c33d493bf892d72feaf0a3372
liuyandong <554030404@qq.com>
2018-01-28 00:37:34 +0800

    增加验证方法！！

--- catering-db.sql
+++ catering-db.sql
@@ -815,3 +815,27 @@ CREATE TABLE vip
     last_update_remark VARCHAR(255) COMMENT '最后更新备注',
     deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-为删除，1-已删除'
 );
+
+DROP TABLE IF EXISTS diet_order_activity;
+CREATE TABLE diet_order_activity
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'id',
+    tenant_id BIGINT NOT NULL COMMENT '商户ID',
+	  tenant_code VARCHAR(20) NOT NULL COMMENT '商户编码',
+    branch_id BIGINT NOT NULL COMMENT '门店ID',
+	  diet_order_id BIGINT NOT NULL COMMENT 'diet_order.id',
+    activity_id BIGINT NOT NULL COMMENT '活动id，activity.id',
+    activity_name VARCHAR(20) NOT NULL COMMENT '活动名称，activity.name',
+    activity_type TINYINT NOT NULL COMMENT '活动名称，activity.type',
+    amount DECIMAL(11, 3) NOT NULL COMMENT '金额',
+    local_id VARCHAR(50) COMMENT '本地ID',
+    local_diet_order_id VARCHAR(50) COMMENT '本地订单ID，local_diet_order.local_id',
+    local_create_time DATETIME COMMENT '本地创建时间',
+    local_last_update_time DATETIME COMMENT '本地最后更新时间',
+    create_time DATETIME DEFAULT now() NOT NULL COMMENT '创建时间',
+    create_user_id BIGINT NOT NULL COMMENT '创建人id',
+    last_update_time DATETIME DEFAULT now() ON UPDATE now() NOT NULL COMMENT '最后更新时间',
+    last_update_user_id BIGINT NOT NULL COMMENT '最后更新人id',
+    last_update_remark VARCHAR(255) COMMENT '最后更新备注',
+    deleted TINYINT DEFAULT 0 NOT NULL COMMENT '是否删除，0-未删除，1-已删除'
+) COMMENT '订单参与的活动';
