611fd937a6bafe140f4876566064b1763a7629e3
liuyandong <554030404@qq.com>
2018-01-30 21:55:37 +0800

    增加验证方法！！

--- src/main/java/build/dream/common/erp/catering/domains/Branch.java
+++ src/main/java/build/dream/common/erp/catering/domains/Branch.java
@@ -5,14 +5,90 @@ import build.dream.common.basic.BasicDomain;
 import java.math.BigInteger;
 
 public class Branch extends BasicDomain {
-    private String code;
-    private String name;
-    private Integer type;
-    private Integer status;
+    /**
+     * 商户id
+     */
     private BigInteger tenantId;
+    /**
+     * 商户编码
+     */
     private String tenantCode;
+    /**
+     * 门店编码
+     */
+    private String code;
+    /**
+     * 门店名称
+     */
+    private String name;
+    /**
+     * 门店类型，1-总部，2-直营店，3加盟店
+     */
+    private Integer type;
+    /**
+     * 门店状态，1-正常
+     */
+    private Integer status;
+    /**
+     * 省编码
+     */
+    private String provinceCode;
+    /**
+     * 省名称
+     */
+    private String provinceName;
+    /**
+     * 市编码
+     */
+    private String cityCode;
+    /**
+     * 市名称
+     */
+    private String cityName;
+    /**
+     * 区编码
+     */
+    private String districtCode;
+    /**
+     * 区名称
+     */
+    private String districtName;
+    /**
+     * 门店详细地址
+     */
+    private String address;
+    /**
+     * 经度
+     */
+    private String longitude;
+    /**
+     * 纬度
+     */
+    private String latitude;
+    /**
+     * 饿了么账号类型，1-连锁账号，2-独立账号
+     */
     private Integer elemeAccountType;
+    /**
+     * 饿了么门店id
+     */
     private BigInteger shopId;
+
+    public BigInteger getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(BigInteger tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public String getTenantCode() {
+        return tenantCode;
+    }
+
+    public void setTenantCode(String tenantCode) {
+        this.tenantCode = tenantCode;
+    }
 
     public String getCode() {
         return code;
@@ -46,20 +122,76 @@ public class Branch extends BasicDomain {
         this.status = status;
     }
 
-    public BigInteger getTenantId() {
-        return tenantId;
+    public String getProvinceCode() {
+        return provinceCode;
     }
 
-    public void setTenantId(BigInteger tenantId) {
-        this.tenantId = tenantId;
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
     }
 
-    public String getTenantCode() {
-        return tenantCode;
+    public String getProvinceName() {
+        return provinceName;
     }
 
-    public void setTenantCode(String tenantCode) {
-        this.tenantCode = tenantCode;
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getDistrictCode() {
+        return districtCode;
+    }
+
+    public void setDistrictCode(String districtCode) {
+        this.districtCode = districtCode;
+    }
+
+    public String getDistrictName() {
+        return districtName;
+    }
+
+    public void setDistrictName(String districtName) {
+        this.districtName = districtName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
     }
 
     public Integer getElemeAccountType() {
