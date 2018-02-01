4981241639acfef409aabab8d1d054fb634f1fd2
liuyandong <554030404@qq.com>
2018-02-01 10:48:59 +0800

    微信开发！！

--- catering-db.sql
+++ catering-db.sql
@@ -616,6 +616,26 @@ CREATE TABLE full_reduction_activity
     deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
 );
 
+DROP TABLE IF EXISTS special_goods_activity;
+CREATE TABLE special_goods_activity (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'id',
+    tenant_id BIGINT NOT NULL COMMENT '商户ID',
+    tenant_code VARCHAR(20) NOT NULL COMMENT '商户编号',
+    branch_id BIGINT NOT NULL COMMENT '门店ID',
+    activity_id BIGINT NOT NULL COMMENT '活动ID',
+    goods_id BIGINT NOT NULL COMMENT '商品id',
+    goods_specification_id BIGINT NOT NULL COMMENT '商品规格id',
+    discount_type TINYINT NOT NULL COMMENT '优惠方式，1-特价，2-折扣',
+    special_price DECIMAL(11, 3) COMMENT '特价金额',
+    discount_rate DECIMAL(5, 2) COMMENT '折扣率',
+    create_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
+    create_user_id BIGINT(20) NOT NULL COMMENT '创建人id',
+    last_update_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
+    last_update_user_id BIGINT NOT NULL COMMENT '最后更新人id',
+    last_update_remark VARCHAR(255) COMMENT '最后更新备注',
+    deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
+) COMMENT '特价商品活动';
+
 DROP PROCEDURE IF EXISTS proc_find_all_activities;
 CREATE PROCEDURE proc_find_all_activities()
 BEGIN
