cca5f84466e6729d3d9a4eda4ccf831063c358ef
liuyandong <554030404@qq.com>
2018-02-02 21:13:45 +0800

    增加验证方法！！

---
+++ src/main/java/build/dream/common/saas/domains/AlipayMaterialImage.java
@@ -0,0 +1,33 @@
+package build.dream.common.saas.domains;
+
+import build.dream.common.basic.BasicDomain;
+
+public class AlipayMaterialImage extends BasicDomain {
+    private String alipayUserId;
+    private String imageId;
+    private String imageUrl;
+
+    public String getAlipayUserId() {
+        return alipayUserId;
+    }
+
+    public void setAlipayUserId(String alipayUserId) {
+        this.alipayUserId = alipayUserId;
+    }
+
+    public String getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(String imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
