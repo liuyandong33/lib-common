d5e99168d1b50e803244816f526810026c1b13ea
liuyandong <554030404@qq.com>
2018-02-10 19:24:47 +0800

    蜂鸟配送对接！！！

--- saas-db.sql
+++ saas-db.sql
@@ -687,3 +687,23 @@ CREATE TABLE alipay_material_image (
     last_update_remark VARCHAR(255) COMMENT '最后更新备注',
     deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
 ) COMMENT = '订单表';
+
+DROP TABLE IF EXISTS sale_flow;
+CREATE TABLE sale_flow
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'id',
+    order_id BIGINT NOT NULL COMMENT '订单id',
+    tenant_id BIGINT NOT NULL COMMENT '商户id',
+    branch_id BIGINT NOT NULL COMMENT '门店id',
+    occurrence_time DATETIME NOT NULL COMMENT '发生时间',
+    goods_id BIGINT NOT NULL COMMENT '商品id',
+    goods_name VARCHAR(20) NOT NULL COMMENT '产品名称',
+    goods_specification_id BIGINT NOT NULL COMMENT '商品规格id',
+    goods_specification_name VARCHAR(20) NOT NULL COMMENT '产品名称',
+    create_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
+    create_user_id BIGINT(20) NOT NULL COMMENT '创建人id',
+    last_update_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
+    last_update_user_id BIGINT NOT NULL COMMENT '最后更新人id',
+    last_update_remark VARCHAR(255) COMMENT '最后更新备注',
+    deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
+) COMMENT = '销售流水';
