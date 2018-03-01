03a6ffa891287c61c42b070b474a652f58a0b7b2
liuyandong <554030404@qq.com>
2018-03-01 12:38:17 +0800

    微信支付修改接口修改！！！

--- src/main/java/build/dream/common/saas/domains/Goods.java
+++ src/main/java/build/dream/common/saas/domains/Goods.java
@@ -3,11 +3,30 @@ package build.dream.common.saas.domains;
 import build.dream.common.basic.BasicDomain;
 
 public class Goods extends BasicDomain {
+    /**
+     * 商品名称
+     */
     private String name;
+    /**
+     * 商品类型，1-设备，2-基础服务，3-增值服务
+     */
     private Integer type;
+    /**
+     * 商品状态，1-正常，2-停售
+     */
     private Integer status;
+    /**
+     * 图片路径
+     */
     private String photoUrl;
+    /**
+     * 计量方式，1-按时间，2-按数量
+     */
     private Integer meteringMode;
+    /**
+     * 业态，1-餐饮，2-零售
+     */
+    private String business;
 
     public String getName() {
         return name;
@@ -47,5 +66,13 @@ public class Goods extends BasicDomain {
 
     public void setMeteringMode(Integer meteringMode) {
         this.meteringMode = meteringMode;
+    }
+
+    public String getBusiness() {
+        return business;
+    }
+
+    public void setBusiness(String business) {
+        this.business = business;
     }
 }
