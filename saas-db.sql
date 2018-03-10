dac2317ac9a45b785bd1d83579970f79c884e068
liuyandong <554030404@qq.com>
2018-03-11 00:40:00 +0800

    代理商合同开发！！

--- saas-db.sql
+++ saas-db.sql
@@ -777,3 +777,20 @@ CREATE TABLE goods_type
     last_update_remark VARCHAR(255) COMMENT '最后更新备注',
     deleted TINYINT DEFAULT 0 NOT NULL COMMENT '是否删除，0-未删除，1-已删除'
 ) COMMENT '商品类型表';
+
+DROP TABLE IF EXISTS agent_contract;
+CREATE TABLE agent_contract
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
+    contract_number VARCHAR(20) NOT NULL COMMENT '合同编号',
+    agent_id BIGINT NOT NULL COMMENT '代理商ID',
+    start_time DATETIME NOT NULL COMMENT '开始时间',
+    end_time DATETIME NOT NULL COMMENT '结束时间',
+    status TINYINT NOT NULL COMMENT '合同状态，1-未审核，2-已审核，3-未执行，4-执行中，5-已终止，6-已过期',
+    create_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
+    create_user_id BIGINT NOT NULL COMMENT '创建人id',
+    last_update_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
+    last_update_user_id BIGINT NOT NULL COMMENT '最后更新人id',
+    last_update_remark VARCHAR(255) COMMENT '最后更新备注',
+    deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
+) COMMENT '代理商合同';
---
+++ src/main/java/build/dream/common/saas/domains/AgentContract.java
@@ -0,0 +1,69 @@
+package build.dream.common.saas.domains;
+
+import build.dream.common.basic.BasicDomain;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+public class AgentContract extends BasicDomain {
+    /**
+     * 合同编号
+     */
+    private String contractNumber;
+    /**
+     * 代理商ID
+     */
+    private BigInteger agentId;
+    /**
+     * 开始时间
+     */
+    private Date startTime;
+    /**
+     * 结束时间
+     */
+    private Date endTime;
+    /**
+     * 合同状态，1-未审核，2-已审核，3-未执行，4-执行中，5-已终止，6-已过期
+     */
+    private Integer status;
+
+    public String getContractNumber() {
+        return contractNumber;
+    }
+
+    public void setContractNumber(String contractNumber) {
+        this.contractNumber = contractNumber;
+    }
+
+    public BigInteger getAgentId() {
+        return agentId;
+    }
+
+    public void setAgentId(BigInteger agentId) {
+        this.agentId = agentId;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
