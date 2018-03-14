e43ff57eba54c8efa8f8f5eefcdc6a09d3a4a7fd
liuyandong <554030404@qq.com>
2018-03-14 20:21:11 +0800

    代理商合同开发！！

--- src/main/java/build/dream/common/constants/Constants.java
+++ src/main/java/build/dream/common/constants/Constants.java
@@ -55,6 +55,7 @@ public class Constants {
     public static final String SERVICE_NAME_POSAPI = "posapi";
     public static final String SERVICE_NAME_APPAPI = "appapi";
     public static final String SERVICE_NAME_PORTAL = "portal";
+    public static final String SERVICE_NAME_O2O = "o2o";
 
     public static final String BUSINESS_CATERING = "1";
     public static final String BUSINESS_RETAIL = "2";
