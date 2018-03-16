ddd1fc712135616848af4e6c53bc73a5eb0fb888
liuyandong <554030404@qq.com>
2018-03-16 14:21:38 +0800

    微信支付修改接口修改！！！

--- src/main/java/build/dream/common/utils/AESUtils.java
+++ src/main/java/build/dream/common/utils/AESUtils.java
@@ -1,7 +1,9 @@
 package build.dream.common.utils;
 
 import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
@@ -12,6 +14,7 @@ import java.security.NoSuchProviderException;
 public class AESUtils {
     public static final String KEY_ALGORITHM = "AES";
     public static final String ALGORITHM_AES_ECB_PKCS7PADDING = "AES/ECB/PKCS7Padding";
+    public static final String ALGORITHM_AES_CBC_PKCS7PADDING = "AES/CBC/PKCS7Padding";
     public static final String PROVIDER_NAME_BC = "BC";
 
     public static byte[] encrypt(byte[] data, byte[] aesKey, String algorithm, String providerName) throws NoSuchPaddingException, NoSuchAlgorithmException, NoSuchProviderException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
@@ -39,6 +42,14 @@ public class AESUtils {
         SecretKey secretKey = new SecretKeySpec(aesKey, KEY_ALGORITHM);
         Cipher cipher = Cipher.getInstance(algorithm);
         cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        return cipher.doFinal(encryptedData);
+    }
+
+    public static byte[] decrypt(byte[] encryptedData, byte[] aesKey, byte[] iv, String algorithm, String providerName) throws NoSuchPaddingException, NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        SecretKey secretKey = new SecretKeySpec(aesKey, KEY_ALGORITHM);
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+        Cipher cipher = Cipher.getInstance(algorithm, providerName);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, ivParameterSpec);
         return cipher.doFinal(encryptedData);
     }
 }
