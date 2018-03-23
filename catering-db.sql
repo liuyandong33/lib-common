4ca5b5d27ec050c421e79ccf3f44a2819c701f50
liuyandong <554030404@qq.com>
2018-03-24 00:00:53 +0800

    代理商合同开发！！

--- catering-db.sql
+++ catering-db.sql
@@ -857,3 +857,21 @@ CREATE TABLE eleme_callback_message
     signature VARCHAR(50) NOT NULL COMMENT 'signature',
     user_id BIGINT NOT NULL COMMENT 'user id'
 ) COMMENT '饿了么催单消息';
+
+DROP TABLE IF EXISTS pos;
+CREATE TABLE pos
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
+    tenant_id BIGINT NOT NULL COMMENT '商户ID',
+    tenant_code VARCHAR(20) NOT NULL COMMENT '商户编号',
+    branch_id BIGINT NOT NULL COMMENT '门店ID',
+    branch_code VARCHAR(20) NOT NULL COMMENT '门店编码',
+    user_id BIGINT NOT NULL COMMENT '用户ID',
+    registration_id VARCHAR(50) NOT NULL COMMENT 'jpush 注册Id',
+    create_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
+    create_user_id BIGINT NOT NULL COMMENT '创建人id',
+    last_update_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
+    last_update_user_id BIGINT NOT NULL COMMENT '最后更新人id',
+    last_update_remark VARCHAR(255) COMMENT '最后更新备注',
+    deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
+) COMMENT 'POS信息表';
---
+++ src/main/java/build/dream/common/erp/catering/domains/Pos.java
@@ -0,0 +1,62 @@
+package build.dream.common.erp.catering.domains;
+
+import build.dream.common.basic.BasicDomain;
+
+import java.math.BigInteger;
+
+public class Pos extends BasicDomain {
+    private BigInteger tenantId;
+    private String tenantCode;
+    private BigInteger branchId;
+    private String branchCode;
+    private BigInteger userId;
+    private String registrationId;
+
+    public BigInteger getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(BigInteger tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public String getTenantCode() {
+        return tenantCode;
+    }
+
+    public void setTenantCode(String tenantCode) {
+        this.tenantCode = tenantCode;
+    }
+
+    public BigInteger getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(BigInteger branchId) {
+        this.branchId = branchId;
+    }
+
+    public String getBranchCode() {
+        return branchCode;
+    }
+
+    public void setBranchCode(String branchCode) {
+        this.branchCode = branchCode;
+    }
+
+    public BigInteger getUserId() {
+        return userId;
+    }
+
+    public void setUserId(BigInteger userId) {
+        this.userId = userId;
+    }
+
+    public String getRegistrationId() {
+        return registrationId;
+    }
+
+    public void setRegistrationId(String registrationId) {
+        this.registrationId = registrationId;
+    }
+}
