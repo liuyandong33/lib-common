96685528553777f8165a1880960d8b07653ea373
liuyandong <554030404@qq.com>
2018-04-25 13:18:26 +0800

    促销活动修改！！

--- src/main/java/build/dream/common/controllers/BasicController.java
+++ src/main/java/build/dream/common/controllers/BasicController.java
@@ -4,5 +4,5 @@ public class BasicController {
     @Deprecated
     public String controllerSimpleName = this.getClass().getSimpleName();
     public String className = this.getClass().getName();
-    public String classSimpleName = this.getClass().getName();
+    public String classSimpleName = this.getClass().getSimpleName();
 }
