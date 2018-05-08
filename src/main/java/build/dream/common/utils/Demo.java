dbf103f30ca35da46341fc180d6fd95dbfca07ea
liuyandong <554030404@qq.com>
2018-05-08 18:07:28 +0800

    促销活动修改！！

--- src/main/java/build/dream/common/utils/Demo.java
+++ src/main/java/build/dream/common/utils/Demo.java
@@ -12,41 +12,26 @@ public class Demo {
     public static void main(String[] args) throws IOException {
 //        SpringApplication.run(Application.class, args);
 
-        InputStream inputStream = new FileInputStream("sign.json");
-        String sign = WebUtils.inputStreamToString(inputStream);
-        JSONObject signJsonObject = JSONObject.fromObject(sign);
+        String userId = "6768100064";
+        String maxBehotTime = "1528442279";
 
-        String maxBehotTime = "1525190399";
 
-        String result = callTouTiaoSystem(maxBehotTime, signJsonObject.getString(maxBehotTime));
+        String result = callTouTiaoSystem(userId, maxBehotTime, "BA8PlhATXwSuIE8WCYrfKgQPDo");
         System.out.println(result);
-        JSONObject resultJsonObject = JSONObject.fromObject(result);
-
-        maxBehotTime = resultJsonObject.getJSONObject("next").getString("max_behot_time");
-
-        result = callTouTiaoSystem(maxBehotTime, signJsonObject.getString(maxBehotTime));
-        System.out.println(result);
-        resultJsonObject = JSONObject.fromObject(result);
-
-        maxBehotTime = resultJsonObject.getJSONObject("next").getString("max_behot_time");
-
-        result = callTouTiaoSystem(maxBehotTime, signJsonObject.getString(maxBehotTime));
-        System.out.println(result);
-        resultJsonObject = JSONObject.fromObject(result);
     }
 
-    public static String callTouTiaoSystem(String maxBehotTime, String signature) throws IOException {
+    public static String callTouTiaoSystem(String userId, String maxBehotTime, String signature) throws IOException {
         Map<String, String> headers = new HashMap<String, String>();
-        headers.put("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36");
+        headers.put("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36");
 
         Map<String, String> requestParameters = new HashMap<String, String>();
         requestParameters.put("page_type", "1");
-        requestParameters.put("user_id", "6768100064");
+        requestParameters.put("user_id", userId);
         requestParameters.put("max_behot_time", maxBehotTime);
         requestParameters.put("count", "20");
         requestParameters.put("tadrequire", "true");
-        requestParameters.put("as", "A1954AAFE0F5977");
-        requestParameters.put("cp", "5AF095E95787AE1");
+        requestParameters.put("as", "A165BADF912673F");
+        requestParameters.put("cp", "5AF1F667538FEE1");
         requestParameters.put("_signature", signature);
         String result = WebUtils.doGetWithRequestParameters("https://www.toutiao.com/c/user/article/", headers, requestParameters);
         return result;
