64ba4cf5c2bcaf598710f89001320dd526c44f0e
liuyandong <554030404@qq.com>
2018-05-21 15:42:23 +0800

    促销活动修改！！

---
+++ src/main/java/build/dream/common/admin/domains/KafkaNode.java
@@ -0,0 +1,92 @@
+package build.dream.common.admin.domains;
+
+import build.dream.common.basic.BasicDomain;
+
+import java.math.BigInteger;
+
+public class KafkaNode extends BasicDomain {
+    /**
+     * 集群ID
+     */
+    private BigInteger clusterId;
+    /**
+     * 主机名称
+     */
+    private String hostName;
+    /**
+     * ip 地址
+     */
+    private String ipAddress;
+    /**
+     * ssh 连接端口号
+     */
+    private Integer sshPort;
+    /**
+     * 用户名
+     */
+    private String userName;
+    /**
+     * 密码
+     */
+    private String password;
+    /**
+     * zookeeper 主目录
+     */
+    private String kafkaHome;
+
+    public BigInteger getClusterId() {
+        return clusterId;
+    }
+
+    public void setClusterId(BigInteger clusterId) {
+        this.clusterId = clusterId;
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public Integer getSshPort() {
+        return sshPort;
+    }
+
+    public void setSshPort(Integer sshPort) {
+        this.sshPort = sshPort;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getKafkaHome() {
+        return kafkaHome;
+    }
+
+    public void setKafkaHome(String kafkaHome) {
+        this.kafkaHome = kafkaHome;
+    }
+}
