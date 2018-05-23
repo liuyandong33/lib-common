6605c52263d408c2ca8fbb9a3bf2b65242da94c9
liuyandong <554030404@qq.com>
2018-05-23 22:22:10 +0800

    代理商合同开发！！

---
+++ src/main/java/build/dream/common/utils/DeleteModel.java
@@ -0,0 +1,53 @@
+package build.dream.common.utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DeleteModel {
+    private String tableName;
+    private List<SearchCondition> searchConditions = new ArrayList<SearchCondition>();
+    private String whereClause;
+    private Map<String, Object> namedParameters = new HashMap<String, Object>();
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public List<SearchCondition> getSearchConditions() {
+        return searchConditions;
+    }
+
+    public void setSearchConditions(List<SearchCondition> searchConditions) {
+        this.searchConditions = searchConditions;
+    }
+
+    public String getWhereClause() {
+        return whereClause;
+    }
+
+    public void setWhereClause(String whereClause) {
+        this.whereClause = whereClause;
+    }
+
+    public Map<String, Object> getNamedParameters() {
+        return namedParameters;
+    }
+
+    public void setNamedParameters(Map<String, Object> namedParameters) {
+        this.namedParameters = namedParameters;
+    }
+
+    public void addNamedParameter(String name, Object value) {
+        this.namedParameters.put(name, value);
+    }
+
+    public void addSearchCondition(String columnName, String operationSymbol, Object searchParameter) {
+        searchConditions.add(new SearchCondition(columnName, operationSymbol, searchParameter));
+    }
+}
