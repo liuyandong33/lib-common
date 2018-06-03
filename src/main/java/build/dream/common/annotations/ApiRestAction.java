2fd335e4393f8c9935db96f847931ff1ddebef2e
liuyandong <554030404@qq.com>
2018-06-03 18:52:08 +0800

    代理商合同开发！！

--- src/main/java/build/dream/common/annotations/ApiRestAction.java
+++ src/main/java/build/dream/common/annotations/ApiRestAction.java
@@ -5,6 +5,6 @@ import java.lang.annotation.*;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface Action {
+public @interface ApiRestAction {
     String error() default "";
 }
