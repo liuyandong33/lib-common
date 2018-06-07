9e3f88631413129d70e3406453b7686e27fd4433
liuyandong <554030404@qq.com>
2018-06-08 01:44:44 +0800

    代理商合同开发！！

--- src/main/java/build/dream/common/utils/Interceptor.java
+++
@@ -1,9 +0,0 @@
-package build.dream.common.utils;
-
-import org.springframework.cglib.proxy.MethodProxy;
-
-import java.lang.reflect.Method;
-
-public interface Interceptor {
-    Object intercept(Object object, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable;
-}
