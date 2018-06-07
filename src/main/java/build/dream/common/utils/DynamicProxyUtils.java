7561b37f6b8154ad5568066aaac325e969be57d0
liuyandong <554030404@qq.com>
2018-06-08 01:43:27 +0800

    代理商合同开发！！

--- src/main/java/build/dream/common/utils/DynamicProxyUtils.java
+++ src/main/java/build/dream/common/utils/DynamicProxyUtils.java
@@ -1,25 +1,13 @@
 package build.dream.common.utils;
 
-import build.dream.common.saas.domains.Tenant;
-import org.springframework.cglib.proxy.MethodProxy;
-
-import java.lang.reflect.Method;
-import java.util.UUID;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
 
 public class DynamicProxyUtils {
-    public static <T> T buildProxyObject(Class<T> clazz, Interceptor interceptor) {
-        Proxy<T> proxy = new Proxy<T>(clazz, interceptor);
-        return proxy.getObject();
-    }
-
-    public static void main(String[] args) {
-        Tenant tenant = DynamicProxyUtils.buildProxyObject(Tenant.class, new Interceptor() {
-            @Override
-            public Object intercept(Object object, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
-                return methodProxy.invokeSuper(object, objects);
-            }
-        });
-
-        tenant.setBusiness(UUID.randomUUID().toString());
+    public static <T> T buildProxyObject(Class<T> clazz, MethodInterceptor methodInterceptor) {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(clazz);
+        enhancer.setCallback(methodInterceptor);
+        return (T) enhancer.create();
     }
 }
