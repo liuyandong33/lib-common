26b468e25470ad4a9b22e0fc38026eb3113420c8
liuyandong <554030404@qq.com>
2018-06-08 21:13:55 +0800

    代理商合同开发！！

--- src/main/java/build/dream/common/constants/Constants.java
+++ src/main/java/build/dream/common/constants/Constants.java
@@ -231,4 +231,6 @@ public class Constants {
     public static final String OK = "OK";
 
     public static final String ISO8601_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+    public static final int DEVICE_TYPE_ANDROID = 1;
+    public static final int DEVICE_TYPE_IOS = 2;
 }
---
+++ src/main/java/build/dream/common/utils/AlibabaCloudMobilePushUtils.java
@@ -0,0 +1,65 @@
+package build.dream.common.utils;
+
+import build.dream.common.constants.Constants;
+import net.sf.json.JSONObject;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.digest.HmacUtils;
+import org.apache.commons.lang.StringUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class AlibabaCloudMobilePushUtils {
+    public static JSONObject pushMessageToAndroid(String accessKeyId, String accessKeySecret, String appKey, String target, String targetValue, String title, String body) throws IOException {
+        return pushMessage(Constants.DEVICE_TYPE_ANDROID, accessKeyId, accessKeySecret, appKey, target, targetValue, title, body);
+    }
+
+    public static JSONObject pushMessageToIos(String accessKeyId, String accessKeySecret, String appKey, String target, String targetValue, String title, String body) throws IOException {
+        return pushMessage(Constants.DEVICE_TYPE_IOS, accessKeyId, accessKeySecret, appKey, target, targetValue, title, body);
+    }
+
+    public static JSONObject pushMessage(int deviceType, String accessKeyId, String accessKeySecret, String appKey, String target, String targetValue, String title, String body) throws IOException {
+        Map<String, String> requestParameters = new HashMap<String, String>();
+        requestParameters.put("Format", Constants.JSON);
+        requestParameters.put("RegionId", "cn-hangzhou");
+        requestParameters.put("Version", "2016-08-01");
+        requestParameters.put("AccessKeyId", accessKeyId);
+        requestParameters.put("SignatureMethod", "HMAC-SHA1");
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(Constants.ISO8601_DATE_FORMAT);
+        requestParameters.put("Timestamp", simpleDateFormat.format(new Date()));
+
+        requestParameters.put("SignatureVersion", "1.0");
+        requestParameters.put("SignatureNonce", UUID.randomUUID().toString());
+        if (deviceType == Constants.DEVICE_TYPE_ANDROID) {
+            requestParameters.put("Action", "PushMessageToAndroid");
+        } else if (deviceType == Constants.DEVICE_TYPE_IOS) {
+            requestParameters.put("Action", "PushMessageToiOS");
+        }
+        requestParameters.put("AppKey", appKey);
+        requestParameters.put("Target", target);
+        requestParameters.put("TargetValue", targetValue);
+        requestParameters.put("Title", title);
+        requestParameters.put("Body", body);
+        requestParameters.put("Signature", calculateSignature(accessKeySecret, requestParameters));
+
+        String url = "http://cloudpush.aliyuncs.com?" + WebUtils.buildQueryString(requestParameters);
+        String result = OutUtils.doGet(url, null);
+
+        JSONObject resultJsonObject = JSONObject.fromObject(result);
+        return resultJsonObject;
+    }
+
+    public static String calculateSignature(String accessKeySecret, Map<String, String> requestParameters) throws UnsupportedEncodingException {
+        Map<String, String> sortedRequestParameters = new TreeMap<String, String>(requestParameters);
+        List<String> pairs = new ArrayList<String>();
+        for (Map.Entry<String, String> entry : sortedRequestParameters.entrySet()) {
+            pairs.add(URLEncoder.encode(entry.getKey(), Constants.CHARSET_NAME_UTF_8) + "=" + URLEncoder.encode(entry.getValue(), Constants.CHARSET_NAME_UTF_8));
+        }
+        String signature = Base64.encodeBase64String(HmacUtils.hmacSha1(accessKeySecret.getBytes(Constants.CHARSET_NAME_UTF_8), StringUtils.join(pairs, "&").getBytes(Constants.CHARSET_NAME_UTF_8)));
+        return signature;
+    }
+}
