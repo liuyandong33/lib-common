e68d1b2569d3e3d68188b164a4089376ac09e135
liuyandong <554030404@qq.com>
2018-06-10 20:19:59 +0800

    代理商合同开发！！

--- src/main/java/build/dream/common/Application.java
+++ src/main/java/build/dream/common/Application.java
@@ -11,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InstantiationException, InterruptedException {
+    public static void main(String[] args) {
 //        SpringApplication.run(Application.class, args);
 
         ClassWriter classWriter = new ClassWriter(0);
