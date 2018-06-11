1114a057b1a113e9aa6cf4c87420808001d2c2e1
liuyandong <554030404@qq.com>
2018-06-11 13:08:37 +0800

    促销活动修改！！

--- src/main/java/build/dream/common/Application.java
+++ src/main/java/build/dream/common/Application.java
@@ -6,6 +6,8 @@ import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.MethodVisitor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.UUID;
+
 /**
  * Created by liuyandong on 2017/7/25.
  */
@@ -16,16 +18,26 @@ public class Application {
 
         ClassWriter classWriter = new ClassWriter(0);
         String className = "build/dream/common/HelloWorld";
-        classWriter.visit(Opcodes.V1_7, Opcodes.ACC_PUBLIC, className, null, "java/lang/Object", null);
-
+        classWriter.visit(Opcodes.V1_8, Opcodes.ACC_PUBLIC, className, null, "java/lang/Object", null);
 
         MethodVisitor constructorMethodVisitor = classWriter.visitMethod(Opcodes.ACC_PUBLIC, "<init>", "()V", null, null);
-        constructorMethodVisitor.visitCode();
         constructorMethodVisitor.visitVarInsn(Opcodes.ALOAD, 0);
-        constructorMethodVisitor.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Object", "<init>", "V()");
+        //执行父类的init初始化
+        constructorMethodVisitor.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
+        //从当前方法返回void
         constructorMethodVisitor.visitInsn(Opcodes.RETURN);
         constructorMethodVisitor.visitMaxs(1, 1);
-        constructorMethodVisitor.visitEnd();
+
+        MethodVisitor runMethodVisitor = classWriter.visitMethod(Opcodes.ACC_PUBLIC, "run", "()V", null, null);
+        //先获取一个java.io.PrintStream对象
+        runMethodVisitor.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+        //将int, float或String型常量值从常量池中推送至栈顶  (此处将message字符串从常量池中推送至栈顶[输出的内容])
+        runMethodVisitor.visitLdcInsn(UUID.randomUUID().toString());
+        //执行println方法（执行的是参数为字符串，无返回值的println函数）
+        runMethodVisitor.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
+        runMethodVisitor.visitInsn(Opcodes.RETURN);
+        runMethodVisitor.visitMaxs(1, 1);
+        runMethodVisitor.visitEnd();
 
         classWriter.visitEnd();
         byte[] code = classWriter.toByteArray();
