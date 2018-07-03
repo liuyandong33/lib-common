2b4b9b4e9212b6da5246973d54a8545588825cc3
liuyandong <554030404@qq.com>
2018-07-03 11:05:42 +0800

    促销活动修改！！

--- src/main/java/build/dream/common/utils/CommonUtils.java
+++ src/main/java/build/dream/common/utils/CommonUtils.java
@@ -1,15 +1,10 @@
 package build.dream.common.utils;
 
 import build.dream.common.constants.Constants;
-import build.dream.common.saas.domains.SystemUser;
-import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang.StringUtils;
 
 import java.io.IOException;
 import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class CommonUtils {
     public static String getServiceName(String business) {
@@ -22,26 +17,19 @@ public class CommonUtils {
         return serviceName;
     }
 
-    public static void loadServiceSystemUsers(List<SystemUser> systemUsers) throws IOException {
-        CacheUtils.delete(Constants.KEY_SERVICE_SYSTEM_USERS);
-        Map<String, String> serviceSystemUserMap = new HashMap<String, String>();
-        for (SystemUser systemUser : systemUsers) {
-            serviceSystemUserMap.put(systemUser.getName(), systemUser.getId().toString());
-        }
-        if (MapUtils.isNotEmpty(serviceSystemUserMap)) {
-            CacheUtils.hmset(Constants.KEY_SERVICE_SYSTEM_USERS, serviceSystemUserMap);
-        }
-    }
-
     public static BigInteger getServiceSystemUserId(String partitionCode, String serviceName) throws IOException {
-        String userId = CacheUtils.hget(Constants.KEY_SERVICE_SYSTEM_USERS, partitionCode + ":" + serviceName);
+        String userId = ConfigurationUtils.getConfiguration(partitionCode + "." + serviceName + ".user.id");
         if (StringUtils.isNotBlank(userId)) {
             return BigInteger.valueOf(Long.valueOf(userId));
         }
         return null;
     }
 
-    public static BigInteger getServiceSystemUserId() throws IOException {
-        return getServiceSystemUserId(PropertyUtils.getProperty(Constants.PARTITION_CODE), PropertyUtils.getProperty(Constants.SERVICE_NAME));
+    public static BigInteger getServiceSystemUserId(String serviceName) throws IOException {
+        String userId = ConfigurationUtils.getConfiguration(serviceName + ".user.id");
+        if (StringUtils.isNotBlank(userId)) {
+            return BigInteger.valueOf(Long.valueOf(userId));
+        }
+        return null;
     }
 }
