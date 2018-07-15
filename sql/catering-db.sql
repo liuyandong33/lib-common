78ffb823ba3bb2ef599eed8acdce9eed9dade361
liuyandong <554030404@qq.com>
2018-07-16 00:43:51 +0800

    代理商合同开发！！

--- sql/catering-db.sql
+++ sql/catering-db.sql
@@ -511,6 +511,28 @@ CREATE TABLE purchase_order
     deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
 ) COMMENT '进货单';
 
+DROP TABLE IF EXISTS purchase_order_detail;
+CREATE TABLE purchase_order_detail
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '主键id',
+    tenant_id BIGINT NOT NULL COMMENT '商户ID',
+    tenant_code VARCHAR(20) NOT NULL COMMENT '商户号',
+    branch_id BIGINT NOT NULL COMMENT '门店ID',
+    purchase_order_id BIGINT NOT NULL COMMENT '进货单ID',
+    goods_id BIGINT NOT NULL COMMENT '商品ID',
+    goods_specification_id BIGINT NOT NULL COMMENT '商品规格ID',
+    unit_id BIGINT NOT NULL COMMENT '商品单位ID',
+    purchase_price DECIMAL(11, 3) NOT NULL COMMENT '进货价',
+    quantity DECIMAL(11, 3) NOT NULL COMMENT '进货数量',
+    create_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
+    create_user_id BIGINT NOT NULL COMMENT '创建人id',
+    last_update_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
+    last_update_user_id BIGINT NOT NULL COMMENT '最后更新人id',
+    last_update_remark VARCHAR(255) NOT NULL COMMENT '最后更新备注',
+    delete_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
+    deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
+) COMMENT '进货单详情';
+
 DROP TABLE IF EXISTS goods_attribute_group;
 CREATE TABLE goods_attribute_group
 (
---
+++ src/main/java/build/dream/common/erp/catering/domains/PurchaseOrderDetail.java
@@ -0,0 +1,88 @@
+package build.dream.common.erp.catering.domains;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class PurchaseOrderDetail {
+    private BigInteger tenantId;
+    private String tenantCode;
+    private BigInteger branchId;
+    private BigInteger purchaseOrderId;
+    private BigInteger goodsId;
+    private BigInteger goodsSpecificationId;
+    private BigInteger unitId;
+    private BigDecimal purchasePrice;
+    private BigDecimal quantity;
+
+    public BigInteger getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(BigInteger tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public String getTenantCode() {
+        return tenantCode;
+    }
+
+    public void setTenantCode(String tenantCode) {
+        this.tenantCode = tenantCode;
+    }
+
+    public BigInteger getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(BigInteger branchId) {
+        this.branchId = branchId;
+    }
+
+    public BigInteger getPurchaseOrderId() {
+        return purchaseOrderId;
+    }
+
+    public void setPurchaseOrderId(BigInteger purchaseOrderId) {
+        this.purchaseOrderId = purchaseOrderId;
+    }
+
+    public BigInteger getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(BigInteger goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public BigInteger getGoodsSpecificationId() {
+        return goodsSpecificationId;
+    }
+
+    public void setGoodsSpecificationId(BigInteger goodsSpecificationId) {
+        this.goodsSpecificationId = goodsSpecificationId;
+    }
+
+    public BigInteger getUnitId() {
+        return unitId;
+    }
+
+    public void setUnitId(BigInteger unitId) {
+        this.unitId = unitId;
+    }
+
+    public BigDecimal getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(BigDecimal purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+}
