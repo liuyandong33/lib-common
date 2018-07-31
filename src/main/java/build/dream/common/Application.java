4559f18ff4d2396a53659f44bee7c445ac33f4d1
liuyandong <554030404@qq.com>
2018-07-31 16:04:27 +0800

    促销活动修改！！

--- src/main/java/build/dream/common/Application.java
+++ src/main/java/build/dream/common/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by liuyandong on 2017/7/25.
  */
 @SpringBootApplication
-public class Application extends ClassLoader {
+public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
