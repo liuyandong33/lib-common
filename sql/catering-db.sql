376c4736254e110855654e9daf9be0c9a1d74a25
liuyandong <554030404@qq.com>
2018-08-05 15:11:31 +0800

    代理商合同开发！！

--- sql/catering-db.sql
+++ sql/catering-db.sql
@@ -1497,3 +1497,19 @@ CREATE TABLE payment
     delete_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
     deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-为删除，1-已删除'
 ) COMMENT '支付方式表';
+
+DROP TABLE IF EXISTS coupon;
+CREATE TABLE coupon
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
+    tenant_id BIGINT NOT NULL COMMENT '商户ID',
+    tenant_code VARCHAR(20) NOT NULL COMMENT '商户编码',
+    branch_id BIGINT NOT NULL COMMENT '门店ID',
+    create_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
+    create_user_id BIGINT NOT NULL COMMENT '创建人id',
+    last_update_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
+    last_update_user_id BIGINT NOT NULL COMMENT '最后更新人id',
+    last_update_remark VARCHAR(255) NOT NULL COMMENT '最后更新备注',
+    delete_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
+    deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
+);
---
+++ src/main/java/build/dream/common/erp/catering/domains/Coupon.java
@@ -0,0 +1,6 @@
+package build.dream.common.erp.catering.domains;
+
+import build.dream.common.basic.BasicDomain;
+
+public class Coupon extends BasicDomain {
+}
