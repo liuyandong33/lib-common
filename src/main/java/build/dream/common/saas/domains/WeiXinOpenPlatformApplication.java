82245ab877ddd0ea1526f71a9bd0428c27781c28
liuyandong <554030404@qq.com>
2018-08-14 14:42:46 +0800

    促销活动修改！！

--- src/main/java/build/dream/common/saas/domains/WeiXinOpenPlatformApplication.java
+++ src/main/java/build/dream/common/saas/domains/WeiXinOpenPlatformApplication.java
@@ -5,6 +5,9 @@ import build.dream.common.basic.BasicDomain;
 public class WeiXinOpenPlatformApplication extends BasicDomain {
     private String appId;
     private String appSecret;
+    private String encodingAesKey;
+    private String token;
+    private Integer type;
 
     public String getAppId() {
         return appId;
@@ -20,5 +23,29 @@ public class WeiXinOpenPlatformApplication extends BasicDomain {
 
     public void setAppSecret(String appSecret) {
         this.appSecret = appSecret;
+    }
+
+    public String getEncodingAesKey() {
+        return encodingAesKey;
+    }
+
+    public void setEncodingAesKey(String encodingAesKey) {
+        this.encodingAesKey = encodingAesKey;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
     }
 }
