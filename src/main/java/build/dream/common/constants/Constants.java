15c0bdea96a7307da0706a1d59126450426af52c
liuyandong <554030404@qq.com>
2018-08-23 09:21:16 +0800

    促销活动修改！！

--- src/main/java/build/dream/common/constants/Constants.java
+++ src/main/java/build/dream/common/constants/Constants.java
@@ -331,6 +331,19 @@ public class Constants {
     public static final String SATURDAY = "SATURDAY";
     public static final String SUNDAY = "SUNDAY";
 
+    public static final String JANUARY = "January";
+    public static final String FEBRUARY = "February";
+    public static final String MARCH = "March";
+    public static final String APRIL = "April";
+    public static final String MAY = "May";
+    public static final String JUNE = "June";
+    public static final String JULY = "July";
+    public static final String AUGUST = "August";
+    public static final String SEPTEMBER = "September";
+    public static final String OCTOBER = "October";
+    public static final String NOVEMBER = "November";
+    public static final String DECEMBER = "December";
+
     public static final String KEY_WEI_XIN_COMPONENT_VERIFY_TICKET = "_wei_xin_component_verify_ticket";
     public static final String KEY_WEI_XIN_COMPONENT_ACCESS_TOKEN = "_wei_xin_component_access_token";
 
