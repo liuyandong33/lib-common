0ff616a5836525f394e6bdbd81387ffa4efef0fc
liuyandong <554030404@qq.com>
2018-08-24 16:20:10 +0800

    促销活动修改！！

--- sql/saas-db.sql
+++ sql/saas-db.sql
@@ -878,3 +878,28 @@ CREATE TABLE wei_xin_open_platform_application
     delete_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
     deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
 ) COMMENT '微信开放平台应用';
+
+DROP TABLE IF EXISTS wei_xin_authorizer_info;
+CREATE TABLE wei_xin_authorizer_info
+(
+    id BIGINT(20) PRIMARY KEY NOT NULL COMMENT 'id' AUTO_INCREMENT,
+    tenant_id BIGINT(20) NOT NULL COMMENT '商户ID',
+    nick_name VARCHAR(20) NOT NULL COMMENT '授权方昵称',
+    head_img VARCHAR(255) NOT NULL COMMENT '授权方头像',
+    service_type_info TINYINT NOT NULL COMMENT '授权方公众号类型，0代表订阅号，1代表由历史老帐号升级后的订阅号，2代表服务号',
+    verify_type_info TINYINT NOT NULL COMMENT '授权方认证类型，-1代表未认证，0代表微信认证，1代表新浪微博认证，2代表腾讯微博认证，3代表已资质认证通过但还未通过名称认证，4代表已资质认证通过、还未通过名称认证，但通过了新浪微博认证，5代表已资质认证通过、还未通过名称认证，但通过了腾讯微博认证',
+    original_id VARCHAR(50) NOT NULL COMMENT '授权方公众号的原始ID',
+    principal_name VARCHAR(50) NOT NULL COMMENT '公众号的主体名称',
+    alias VARCHAR(50) NOT NULL COMMENT '授权方公众号所设置的微信号，可能为空',
+    business_info VARCHAR(50) NOT NULL COMMENT '用以了解以下功能的开通状况（0代表未开通，1代表已开通）： open_store:是否开通微信门店功能 open_scan:是否开通微信扫商品功能 open_pay:是否开通微信支付功能 open_card:是否开通微信卡券功能 open_shake:是否开通微信摇一摇功能',
+    qrcode_url VARCHAR(255) NOT NULL COMMENT '二维码图片的URL',
+    authorization_appid VARCHAR(50) NOT NULL COMMENT 'app id',
+    func_info VARCHAR(255) NOT NULL COMMENT '公众号授权给开发者的权限集列表，ID为1到15时分别代表： 1.消息管理权限 2.用户管理权限 3.帐号服务权限 4.网页服务权限 5.微信小店权限 6.微信多客服权限 7.群发与通知权限 8.微信卡券权限 9.微信扫一扫权限 10.微信连WIFI权限 11.素材管理权限 12.微信摇周边权限 13.微信门店权限 14.微信支付权限 15.自定义菜单权限 请注意： 1）该字段的返回不会考虑公众号是否具备该权限集的权限（因为可能部分具备），请根据公众号的帐号类型和认证情况，来判断公众号的接口权限。',
+    create_time DATETIME DEFAULT NOW() NOT NULL COMMENT '创建时间',
+    create_user_id BIGINT(20) NOT NULL COMMENT '创建人id',
+    last_update_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
+    last_update_user_id BIGINT(20) NOT NULL COMMENT '最后更新人id',
+    last_update_remark VARCHAR(255) NOT NULL COMMENT '最后更新备注',
+    delete_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
+    deleted TINYINT(4) DEFAULT '0' NOT NULL COMMENT '是否删除，0-未删除，1-已删除'
+) COMMENT = '微信公众号';
