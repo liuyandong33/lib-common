71313d4b3784e3c8f03eb7e6b5c45eeadfd02119
liuyandong <554030404@qq.com>
2018-08-28 13:11:35 +0800

    促销活动修改！！

--- src/main/java/build/dream/common/utils/WeiXinUtils.java
+++ src/main/java/build/dream/common/utils/WeiXinUtils.java
@@ -345,7 +345,7 @@ public class WeiXinUtils {
 
         Map<String, Object> authorizationInfo = MapUtils.getMap(result, "authorization_info");
         String authorizerAppId = MapUtils.getString(authorizationInfo, "authorizer_appid");
-        String authorizerAccessToken = MapUtils.getString(authorizationInfo, "authorizer_appid");
+        String authorizerAccessToken = MapUtils.getString(authorizationInfo, "authorizer_access_token");
         int expiresIn = MapUtils.getIntValue(authorizationInfo, "expires_in");
         String authorizerRefreshToken = MapUtils.getString(authorizationInfo, "authorizer_refresh_token");
         Date fetchTime = new Date();
