b4bf07740d7f431f10a1005afcdb0ecbdefd482a
liuyandong <554030404@qq.com>
2018-08-30 09:06:54 +0800

    促销活动修改！！

--- src/main/java/build/dream/common/utils/ActiveMQUtils.java
+++ src/main/java/build/dream/common/utils/ActiveMQUtils.java
@@ -1,6 +1,10 @@
 package build.dream.common.utils;
 
 import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.messaging.MessagingException;
+import org.springframework.messaging.core.MessagePostProcessor;
+
+import java.util.Map;
 
 public class ActiveMQUtils {
     private static JmsMessagingTemplate jmsMessagingTemplate;
@@ -10,5 +14,53 @@ public class ActiveMQUtils {
             jmsMessagingTemplate = ApplicationHandler.getBean(JmsMessagingTemplate.class);
         }
         return jmsMessagingTemplate;
+    }
+
+    public static void convertAndSend(Object payload) throws MessagingException {
+        obtainJmsMessagingTemplate().convertAndSend(payload);
+    }
+
+    public static void convertAndSend(Object payload, MessagePostProcessor postProcessor) throws MessagingException {
+        obtainJmsMessagingTemplate().convertAndSend(payload, postProcessor);
+    }
+
+    public static void convertAndSend(String destinationName, Object payload) throws MessagingException {
+        obtainJmsMessagingTemplate().convertAndSend(destinationName, payload);
+    }
+
+    public static void convertAndSend(String destinationName, Object payload, Map<String, Object> headers) throws MessagingException {
+        obtainJmsMessagingTemplate().convertAndSend(destinationName, payload, headers);
+    }
+
+    public static void convertAndSend(String destinationName, Object payload, MessagePostProcessor postProcessor) throws MessagingException {
+        obtainJmsMessagingTemplate().convertAndSend(destinationName, payload, postProcessor);
+    }
+
+    public static void convertAndSend(String destinationName, Object payload, Map<String, Object> headers, MessagePostProcessor postProcessor) throws MessagingException {
+        obtainJmsMessagingTemplate().convertAndSend(destinationName, payload, headers, postProcessor);
+    }
+
+    public static <T> T convertSendAndReceive(String destinationName, Object request, Class<T> targetClass) throws MessagingException {
+        return obtainJmsMessagingTemplate().convertSendAndReceive(destinationName, request, targetClass);
+    }
+
+    public static <T> T convertSendAndReceive(Object request, Class<T> targetClass) {
+        return obtainJmsMessagingTemplate().convertSendAndReceive(request, targetClass);
+    }
+
+    public static <T> T convertSendAndReceive(String destinationName, Object request, Map<String, Object> headers, Class<T> targetClass) throws MessagingException {
+        return obtainJmsMessagingTemplate().convertSendAndReceive(destinationName, request, headers, targetClass);
+    }
+
+    public static <T> T convertSendAndReceive(Object request, Class<T> targetClass, MessagePostProcessor postProcessor) {
+        return obtainJmsMessagingTemplate().convertSendAndReceive(request, targetClass, postProcessor);
+    }
+
+    public static <T> T convertSendAndReceive(String destinationName, Object request, Class<T> targetClass, MessagePostProcessor requestPostProcessor) throws MessagingException {
+        return obtainJmsMessagingTemplate().convertSendAndReceive(destinationName, request, targetClass, requestPostProcessor);
+    }
+
+    public static <T> T convertSendAndReceive(String destinationName, Object request, Map<String, Object> headers, Class<T> targetClass, MessagePostProcessor requestPostProcessor) throws MessagingException {
+        return obtainJmsMessagingTemplate().convertSendAndReceive(destinationName, request, headers, targetClass, requestPostProcessor);
     }
 }
