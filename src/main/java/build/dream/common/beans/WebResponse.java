1612d9aa204535df9cef165d2183413101f395c7
liuyandong <554030404@qq.com>
2018-09-10 15:10:57 +0800

    促销活动修改！！

--- src/main/java/build/dream/common/beans/WebResponse.java
+++ src/main/java/build/dream/common/beans/WebResponse.java
@@ -1,11 +1,16 @@
 package build.dream.common.beans;
 
+import build.dream.common.constants.Constants;
+
+import javax.servlet.http.Cookie;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class WebResponse {
     private String result;
-    private Map<String, List<String>> headers;
+    private Map<String, List<String>> headers = new HashMap<String, List<String>>();
+    private Map<String, Cookie> cookieMap = new HashMap<String, Cookie>();
     private int responseCode;
 
     public WebResponse() {
@@ -14,8 +19,48 @@ public class WebResponse {
 
     public WebResponse(String result, Map<String, List<String>> headers, int responseCode) {
         this.result = result;
-        this.headers = headers;
         this.responseCode = responseCode;
+        for (Map.Entry<String, List<String>> header : headers.entrySet()) {
+            String key = header.getKey();
+            List<String> headerValues = header.getValue();
+            if (Constants.SET_COOKIE.equals(key)) {
+                this.headers.put(key, headerValues);
+                for (String headerValue : headerValues) {
+                    String[] array = headerValue.split("; ");
+                    String[] nameAndValue = array[0].split("=");
+                    String name = nameAndValue[0];
+                    String value = nameAndValue[1];
+
+                    Cookie cookie = new Cookie(name, value);
+
+                    int length = array.length;
+                    for (int index = 1; index < length; index++) {
+                        String item = array[index];
+                        if (item.indexOf("=") >= 0) {
+                            String[] attributeNameAndValue = item.split("=");
+                            String attributeName = attributeNameAndValue[0];
+                            String attributeValue = attributeNameAndValue[1];
+                            if ("Comment".equalsIgnoreCase(attributeName)) {
+                                cookie.setComment(attributeValue);
+                            } else if ("Domain".equalsIgnoreCase(attributeName)) {
+                                cookie.setDomain(attributeValue);
+                            } else if ("Max-Age".equalsIgnoreCase(attributeName)) {
+                                cookie.setMaxAge(Integer.parseInt(attributeValue));
+                            } else if ("Path".equalsIgnoreCase(attributeName)) {
+                                cookie.setPath(attributeValue);
+                            }
+                        } else if ("Secure".equalsIgnoreCase(item)) {
+                            cookie.setSecure(true);
+                        } else if ("HttpOnly".equalsIgnoreCase(item)) {
+                            cookie.setHttpOnly(true);
+                        }
+                    }
+                    cookieMap.put(name, cookie);
+                }
+            } else {
+                this.headers.put(key, headerValues);
+            }
+        }
     }
 
     public String getResult() {
