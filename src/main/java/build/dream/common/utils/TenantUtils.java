f036c1a174ab462bc5966e6c395cab40c5157464
liuyandong <554030404@qq.com>
2018-09-10 10:08:59 +0800

    促销活动修改！！

---
+++ src/main/java/build/dream/common/utils/TenantUtils.java
@@ -0,0 +1,34 @@
+package build.dream.common.utils;
+
+import build.dream.common.api.ApiRest;
+import build.dream.common.constants.Constants;
+import build.dream.common.saas.domains.Tenant;
+import scala.Tuple2;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TenantUtils {
+    public static Tenant obtainTenantInfo(BigInteger tenantId) throws IOException {
+        Map<String, String> obtainTenantInfoRequestParameters = new HashMap<String, String>();
+        obtainTenantInfoRequestParameters.put("tenantId", tenantId.toString());
+
+        ApiRest apiRest = ProxyUtils.doGetWithRequestParameters(Constants.SERVICE_NAME_PLATFORM, "tenant", "obtainTenantInfo", obtainTenantInfoRequestParameters);
+        ValidateUtils.isTrue(apiRest.isSuccessful(), apiRest.getError());
+        return (Tenant) apiRest.getData();
+    }
+
+    public static Tenant obtainTenantInfo(String tenantCode) throws IOException {
+        Map<String, String> obtainTenantInfoRequestParameters = new HashMap<String, String>();
+        obtainTenantInfoRequestParameters.put("tenantCode", tenantCode);
+        ApiRest apiRest = ProxyUtils.doGetWithRequestParameters(Constants.SERVICE_NAME_PLATFORM, "tenant", "obtainTenantInfo", obtainTenantInfoRequestParameters);
+        ValidateUtils.isTrue(apiRest.isSuccessful(), apiRest.getError());
+        return (Tenant) apiRest.getData();
+    }
+
+    public static void updateTenantInfo(BigInteger tenantId, Tuple2<String, String>... fields) {
+
+    }
+}
