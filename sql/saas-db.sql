cff2d2b218ef1221bbae472091c506232515749b
liuyandong <554030404@qq.com>
2018-09-12 09:57:07 +0800

    促销活动修改！！

--- sql/saas-db.sql
+++ sql/saas-db.sql
@@ -928,3 +928,22 @@ CREATE TABLE wei_xin_authorizer_token
     delete_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
     deleted TINYINT(4) DEFAULT 0 NOT NULL COMMENT '是否删除，0-未删除，1-已删除'
 ) COMMENT = '微信公众号或小程序授权token';
+
+DROP TABLE IF EXISTS new_land_account;
+CREATE TABLE new_land_account
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'id',
+    tenant_id BIGINT NOT NULL COMMENT '商户ID',
+    branch_id BIGINT NOT NULL COMMENT '门店ID',
+    mch_id VARCHAR(50) NOT NULL COMMENT '新大陆支付商户号',
+    trm_no VARCHAR(50) NOT NULL COMMENT '新大陆支付设备号',
+    org_no VARCHAR(50) NOT NULL COMMENT '新大陆支付机构号',
+    `key` VARCHAR(50) NOT NULL COMMENT '新大陆支付密钥',
+    create_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
+    create_user_id BIGINT NOT NULL COMMENT '创建人id',
+    last_update_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
+    last_update_user_id BIGINT NOT NULL COMMENT '最后更新人id',
+    last_update_remark VARCHAR(255) NOT NULL COMMENT '最后更新备注',
+    delete_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
+    deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
+) COMMENT '新大陆账号';
---
+++ src/main/java/build/dream/common/saas/domains/NewLandAccount.java
@@ -0,0 +1,80 @@
+package build.dream.common.saas.domains;
+
+import build.dream.common.basic.BasicDomain;
+
+import java.math.BigInteger;
+
+public class NewLandAccount extends BasicDomain {
+    /**
+     * 商户ID
+     */
+    private BigInteger tenantId;
+    /**
+     * 门店ID
+     */
+    private BigInteger branchId;
+    /**
+     * 新大陆支付商户号
+     */
+    private String mchId;
+    /**
+     * 新大陆支付设备号
+     */
+    private String trmNo;
+    /**
+     * 新大陆支付机构号
+     */
+    private String orgNo;
+    /**
+     * 新大陆支付密钥
+     */
+    private String key;
+
+    public BigInteger getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(BigInteger tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public BigInteger getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(BigInteger branchId) {
+        this.branchId = branchId;
+    }
+
+    public String getMchId() {
+        return mchId;
+    }
+
+    public void setMchId(String mchId) {
+        this.mchId = mchId;
+    }
+
+    public String getTrmNo() {
+        return trmNo;
+    }
+
+    public void setTrmNo(String trmNo) {
+        this.trmNo = trmNo;
+    }
+
+    public String getOrgNo() {
+        return orgNo;
+    }
+
+    public void setOrgNo(String orgNo) {
+        this.orgNo = orgNo;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
