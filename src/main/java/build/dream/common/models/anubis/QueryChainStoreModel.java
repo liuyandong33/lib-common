c316c7a91fd3c7063a62e9f3b1f43925b2765d88
liuyandong <554030404@qq.com>
2018-09-16 20:15:12 +0800

    蜂鸟配送接口开发！！

--- src/main/java/build/dream/common/models/anubis/QueryChainStoreModel.java
+++ src/main/java/build/dream/common/models/anubis/QueryChainStoreModel.java
@@ -1,7 +1,30 @@
 package build.dream.common.models.anubis;
 
 import build.dream.common.models.BasicModel;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
 
 public class QueryChainStoreModel extends BasicModel {
+    @SerializedName(value = "chain_store_code", alternate = "chainStoreCode")
+    private List<String> chainStoreCode;
 
+    @SerializedName(value = "chain_store_name", alternate = "chainStoreName")
+    private String chainStoreName;
+
+    public List<String> getChainStoreCode() {
+        return chainStoreCode;
+    }
+
+    public void setChainStoreCode(List<String> chainStoreCode) {
+        this.chainStoreCode = chainStoreCode;
+    }
+
+    public String getChainStoreName() {
+        return chainStoreName;
+    }
+
+    public void setChainStoreName(String chainStoreName) {
+        this.chainStoreName = chainStoreName;
+    }
 }
