c5414086f6c391b0a34d5d757a9162226c93d752
liuyandong <554030404@qq.com>
2018-09-25 09:54:54 +0800

    促销活动修改！！

--- src/main/java/build/dream/common/utils/TenantUtils.java
+++ src/main/java/build/dream/common/utils/TenantUtils.java
@@ -5,13 +5,12 @@ import build.dream.common.constants.Constants;
 import build.dream.common.saas.domains.Tenant;
 import scala.Tuple2;
 
-import java.io.IOException;
 import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TenantUtils {
-    public static Tenant obtainTenantInfo(BigInteger tenantId) throws IOException {
+    public static Tenant obtainTenantInfo(BigInteger tenantId) {
         Map<String, String> obtainTenantInfoRequestParameters = new HashMap<String, String>();
         obtainTenantInfoRequestParameters.put("tenantId", tenantId.toString());
 
@@ -20,7 +19,7 @@ public class TenantUtils {
         return (Tenant) apiRest.getData();
     }
 
-    public static Tenant obtainTenantInfo(String tenantCode) throws IOException {
+    public static Tenant obtainTenantInfo(String tenantCode) {
         Map<String, String> obtainTenantInfoRequestParameters = new HashMap<String, String>();
         obtainTenantInfoRequestParameters.put("tenantCode", tenantCode);
         ApiRest apiRest = ProxyUtils.doGetWithRequestParameters(Constants.SERVICE_NAME_PLATFORM, "tenant", "obtainTenantInfo", obtainTenantInfoRequestParameters);
@@ -28,7 +27,7 @@ public class TenantUtils {
         return (Tenant) apiRest.getData();
     }
 
-    public static void updateTenantInfo(BigInteger tenantId, Tuple2<String, String>... fields) throws IOException {
+    public static void updateTenantInfo(BigInteger tenantId, Tuple2<String, String>... fields) {
         Map<String, String> updateTenantInfoRequestParameters = new HashMap<String, String>();
         updateTenantInfoRequestParameters.put("id", tenantId.toString());
         for (Tuple2<String, String> field : fields) {
@@ -38,7 +37,7 @@ public class TenantUtils {
         ValidateUtils.isTrue(apiRest.isSuccessful(), apiRest.getError());
     }
 
-    public static void updateTenantInfo(BigInteger tenantId, Map<String, String> fields) throws IOException {
+    public static void updateTenantInfo(BigInteger tenantId, Map<String, String> fields) {
         Map<String, String> updateTenantInfoRequestParameters = new HashMap<String, String>(fields);
         updateTenantInfoRequestParameters.put("id", tenantId.toString());
         ApiRest apiRest = ProxyUtils.doPostWithRequestParameters(Constants.SERVICE_NAME_PLATFORM, "tenant", "updateTenantInfo", updateTenantInfoRequestParameters);
