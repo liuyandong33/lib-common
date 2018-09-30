d217ac091d2f33f6948a22ddf985d7484ebba867
liuyandong <554030404@qq.com>
2018-09-30 15:33:52 +0800

    促销活动修改！！

--- src/main/java/build/dream/common/utils/UserUtils.java
+++ src/main/java/build/dream/common/utils/UserUtils.java
@@ -2,12 +2,15 @@ package build.dream.common.utils;
 
 import build.dream.common.api.ApiRest;
 import build.dream.common.constants.Constants;
+import org.apache.commons.lang.StringUtils;
 
+import java.math.BigInteger;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class UserUtils {
-    public Map<String, Object> obtainUserInfo(String loginName) {
+    public static Map<String, Object> obtainUserInfo(String loginName) {
         Map<String, String> obtainUserInfoRequestParameters = new HashMap<String, String>();
         obtainUserInfoRequestParameters.put("loginName", loginName);
 
@@ -15,5 +18,14 @@ public class UserUtils {
         ValidateUtils.isTrue(apiRest.isSuccessful(), apiRest.getError());
 
         return (Map<String, Object>) apiRest.getData();
+    }
+
+    public static List<Map<String, Object>> batchGetUsers(List<BigInteger> userIds) {
+        Map<String, String> batchGetUsersRequestParameters = new HashMap<String, String>();
+        batchGetUsersRequestParameters.put("userIds", StringUtils.join(userIds, ","));
+
+        ApiRest apiRest = ProxyUtils.doGetWithRequestParameters(Constants.SERVICE_NAME_PLATFORM, "user", "batchGetUsers", batchGetUsersRequestParameters);
+        ValidateUtils.isTrue(apiRest.isSuccessful(), apiRest.getError());
+        return (List<Map<String, Object>>) apiRest.getData();
     }
 }
