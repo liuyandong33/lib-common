eca9d4c3da5ca36cee74faa7346e517b4536f13a
liuyandong <554030404@qq.com>
2018-10-10 17:39:50 +0800

    促销活动修改！！

--- sql/admin-db.sql
+++ sql/admin-db.sql
@@ -76,3 +76,20 @@ CREATE TABLE `host`
     delete_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
     deleted TINYINT DEFAULT 0 NOT NULL COMMENT '是否删除，0-未删除，1-已删除'
 ) COMMENT '主机';
+
+DROP TABLE IF EXISTS my_sql_configuration;
+CREATE TABLE my_sql_configuration
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'id',
+    `name` VARCHAR(200) NOT NULL COMMENT '名称',
+    `value` VARCHAR(200) NOT NULL COMMENT '值',
+    `type` VARCHAR(20) NOT NULL COMMENT 'mysqld, client',
+    remark VARCHAR(255) NOT NULL COMMENT '备注',
+    create_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
+    create_user_id BIGINT NOT NULL COMMENT '创建人id',
+    last_update_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
+    last_update_user_id BIGINT NOT NULL COMMENT '最后更新人id',
+    last_update_remark VARCHAR(255) NOT NULL COMMENT '最后更新备注',
+    delete_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
+    deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
+) COMMENT 'mysql 配置';
