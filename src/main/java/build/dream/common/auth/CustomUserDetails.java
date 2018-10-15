74bc3e401fd4165a7dd4aee16b9186a6289b254f
liuyandong <554030404@qq.com>
2018-10-15 18:05:21 +0800

    促销活动修改！！

---
+++ src/main/java/build/dream/common/auth/CustomUserDetails.java
@@ -0,0 +1,107 @@
+package build.dream.common.auth;
+
+import build.dream.common.saas.domains.SystemUser;
+import build.dream.common.saas.domains.Tenant;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class CustomUserDetails implements UserDetails {
+    private Collection<GrantedAuthority> authorities;
+    private String password;
+    private String username;
+    private SystemUser systemUser;
+    private Tenant tenant;
+    private Map<String, Object> branchInfo;
+    private String publicKey;
+    private String privateKey;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Collection<GrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public SystemUser getSystemUser() {
+        return systemUser;
+    }
+
+    public void setSystemUser(SystemUser systemUser) {
+        this.systemUser = systemUser;
+    }
+
+    public Tenant getTenant() {
+        return tenant;
+    }
+
+    public void setTenant(Tenant tenant) {
+        this.tenant = tenant;
+    }
+
+    public Map<String, Object> getBranchInfo() {
+        return branchInfo;
+    }
+
+    public void setBranchInfo(Map<String, Object> branchInfo) {
+        this.branchInfo = branchInfo;
+    }
+
+    public String getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(String publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    public String getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(String privateKey) {
+        this.privateKey = privateKey;
+    }
+}
