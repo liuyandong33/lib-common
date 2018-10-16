fd9c31e4c4e5d14ede72589e18b66048987637b9
liuyandong <554030404@qq.com>
2018-10-16 10:05:28 +0800

    促销活动修改！！

--- src/main/java/build/dream/common/models/BasicModel.java
+++ src/main/java/build/dream/common/models/BasicModel.java
@@ -1,6 +1,5 @@
 package build.dream.common.models;
 
-import build.dream.common.models.weixinpay.UnifiedOrderModel;
 import build.dream.common.utils.ValidateUtils;
 
 public class BasicModel {
@@ -10,13 +9,5 @@ public class BasicModel {
 
     public void validateAndThrow() {
         ValidateUtils.validateAndThrow(this);
-    }
-
-    public BasicModel() {
-        int a = 100;
-    }
-
-    public static void main(String[] args) {
-        UnifiedOrderModel unifiedOrderModel = new UnifiedOrderModel();
     }
 }
