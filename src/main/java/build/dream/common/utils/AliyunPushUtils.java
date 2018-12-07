a6377d18b0e539d8a45c1a44a61528bd5026d6c8
liuyandong <554030404@qq.com>
2018-12-07 09:08:32 +0800

    促销活动修改！！

--- src/main/java/build/dream/common/utils/AliyunPushUtils.java
+++ src/main/java/build/dream/common/utils/AliyunPushUtils.java
@@ -23,6 +23,54 @@ public class AliyunPushUtils {
 
     private static final String CLOUD_PUSH_SERVICE_URL = "http://cloudpush.aliyuncs.com";
 
+    private static String calculateSignature(String accessKeySecret, Map<String, String> requestParameters) throws UnsupportedEncodingException {
+        Map<String, String> sortedRequestParameters = new TreeMap<String, String>(requestParameters);
+        List<String> pairs = new ArrayList<String>();
+        for (Map.Entry<String, String> entry : sortedRequestParameters.entrySet()) {
+            pairs.add(URLEncoder.encode(entry.getKey(), Constants.CHARSET_NAME_UTF_8) + "=" + URLEncoder.encode(entry.getValue(), Constants.CHARSET_NAME_UTF_8));
+        }
+        String signature = Base64.encodeBase64String(HmacUtils.hmacSha1(accessKeySecret.getBytes(Constants.CHARSET_NAME_UTF_8), StringUtils.join(pairs, "&").getBytes(Constants.CHARSET_NAME_UTF_8)));
+        return signature;
+    }
+
+    private static Map<String, String> buildCommonRequestParameters() {
+        Map<String, String> commonRequestParameters = new HashMap<String, String>();
+        commonRequestParameters.put("Format", Constants.JSON);
+        commonRequestParameters.put("RegionId", "cn-hangzhou");
+        commonRequestParameters.put("Version", "2018-03-24");
+        commonRequestParameters.put("SignatureMethod", "HMAC-SHA1");
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(Constants.ISO8601_DATE_PATTERN);
+        commonRequestParameters.put("Timestamp", simpleDateFormat.format(new Date()));
+
+        commonRequestParameters.put("SignatureVersion", "1.0");
+        commonRequestParameters.put("SignatureNonce", UUID.randomUUID().toString());
+        return commonRequestParameters;
+    }
+
+    private static Map<String, Object> pushMessage(int deviceType, String accessKeyId, String accessKeySecret, String appKey, String target, String targetValue, String title, String body) throws UnsupportedEncodingException {
+        Map<String, String> requestParameters = new HashMap<String, String>();
+        requestParameters.putAll(buildCommonRequestParameters());
+        requestParameters.put("AccessKeyId", accessKeyId);
+        if (deviceType == Constants.DEVICE_TYPE_ANDROID) {
+            requestParameters.put("Action", "PushMessageToAndroid");
+        } else if (deviceType == Constants.DEVICE_TYPE_IOS) {
+            requestParameters.put("Action", "PushMessageToiOS");
+        }
+        requestParameters.put("AppKey", appKey);
+        requestParameters.put("Target", target);
+        requestParameters.put("TargetValue", targetValue);
+        requestParameters.put("Title", title);
+        requestParameters.put("Body", body);
+        requestParameters.put("Signature", calculateSignature(accessKeySecret, requestParameters));
+
+        WebResponse webResponse = OutUtils.doPostWithRequestParameters(CLOUD_PUSH_SERVICE_URL, requestParameters);
+        String result = webResponse.getResult();
+
+        Map<String, Object> resultMap = JacksonUtils.readValueAsMap(result, String.class, Object.class);
+        return resultMap;
+    }
+
     public static Map<String, Object> pushMessageToAndroid(PushMessageToAndroidModel pushMessageToAndroidModel) throws IOException {
         pushMessageToAndroidModel.validateAndThrow();
         String accessKeyId = null;
@@ -37,45 +85,17 @@ public class AliyunPushUtils {
         return pushMessage(Constants.DEVICE_TYPE_IOS, accessKeyId, accessKeySecret, pushMessageToIosModel.getAppKey(), pushMessageToIosModel.getTarget(), pushMessageToIosModel.getTargetValue(), pushMessageToIosModel.getTitle(), pushMessageToIosModel.getBody());
     }
 
-    private static Map<String, Object> pushMessage(int deviceType, String accessKeyId, String accessKeySecret, String appKey, String target, String targetValue, String title, String body) throws IOException {
+    public static Map<String, Object> listSummaryApps(String accessKeyId, String accessKeySecret) throws UnsupportedEncodingException {
         Map<String, String> requestParameters = new HashMap<String, String>();
-        requestParameters.put("Format", Constants.JSON);
-        requestParameters.put("RegionId", "cn-hangzhou");
-        requestParameters.put("Version", "2016-08-01");
+        requestParameters.putAll(buildCommonRequestParameters());
         requestParameters.put("AccessKeyId", accessKeyId);
-        requestParameters.put("SignatureMethod", "HMAC-SHA1");
-
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(Constants.ISO8601_DATE_PATTERN);
-        requestParameters.put("Timestamp", simpleDateFormat.format(new Date()));
-
-        requestParameters.put("SignatureVersion", "1.0");
-        requestParameters.put("SignatureNonce", UUID.randomUUID().toString());
-        if (deviceType == Constants.DEVICE_TYPE_ANDROID) {
-            requestParameters.put("Action", "PushMessageToAndroid");
-        } else if (deviceType == Constants.DEVICE_TYPE_IOS) {
-            requestParameters.put("Action", "PushMessageToiOS");
-        }
-        requestParameters.put("AppKey", appKey);
-        requestParameters.put("Target", target);
-        requestParameters.put("TargetValue", targetValue);
-        requestParameters.put("Title", title);
-        requestParameters.put("Body", body);
+        requestParameters.put("Action", "SummaryAppInfos");
         requestParameters.put("Signature", calculateSignature(accessKeySecret, requestParameters));
 
-        WebResponse webResponse = OutUtils.doGetWithRequestParameters(CLOUD_PUSH_SERVICE_URL, requestParameters);
+        WebResponse webResponse = OutUtils.doPostWithRequestParameters(CLOUD_PUSH_SERVICE_URL, requestParameters);
         String result = webResponse.getResult();
 
         Map<String, Object> resultMap = JacksonUtils.readValueAsMap(result, String.class, Object.class);
         return resultMap;
-    }
-
-    private static String calculateSignature(String accessKeySecret, Map<String, String> requestParameters) throws UnsupportedEncodingException {
-        Map<String, String> sortedRequestParameters = new TreeMap<String, String>(requestParameters);
-        List<String> pairs = new ArrayList<String>();
-        for (Map.Entry<String, String> entry : sortedRequestParameters.entrySet()) {
-            pairs.add(URLEncoder.encode(entry.getKey(), Constants.CHARSET_NAME_UTF_8) + "=" + URLEncoder.encode(entry.getValue(), Constants.CHARSET_NAME_UTF_8));
-        }
-        String signature = Base64.encodeBase64String(HmacUtils.hmacSha1(accessKeySecret.getBytes(Constants.CHARSET_NAME_UTF_8), StringUtils.join(pairs, "&").getBytes(Constants.CHARSET_NAME_UTF_8)));
-        return signature;
     }
 }
