72831019b4bdf817577f8e848ee776a3f52d8334
liuyandong <554030404@qq.com>
2018-12-17 17:00:57 +0800

    促销活动修改！！

--- sql/catering-db.sql
+++ sql/catering-db.sql
@@ -459,7 +459,7 @@ CREATE TABLE buy_give_activity
     created_user_id BIGINT NOT NULL COMMENT '创建人id',
     updated_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
     updated_user_id BIGINT NOT NULL COMMENT '最后更新人id',
-    updated_remark VARCHAR(255) COMMENT '最后更新备注',
+    updated_remark VARCHAR(255) NOT NULL COMMENT '最后更新备注',
     deleted_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
     deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
 ) COMMENT '买A赠B活动';
@@ -901,7 +901,7 @@ CREATE TABLE diet_order_delivery_state
     created_user_id BIGINT NOT NULL COMMENT '创建人id',
     updated_time DATETIME DEFAULT NOW() ON UPDATE NOW() NOT NULL COMMENT '最后更新时间',
     updated_user_id BIGINT NOT NULL COMMENT '最后更新人id',
-    updated_remark VARCHAR(255) COMMENT '最后更新备注',
+    updated_remark VARCHAR(255) NOT NULL COMMENT '最后更新备注',
     deleted_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
     deleted TINYINT DEFAULT 0 NOT NULL COMMENT '是否删除，0-未删除，1-已删除'
 ) COMMENT '订单配送状态';
@@ -1006,7 +1006,7 @@ CREATE TABLE sale
     created_user_id BIGINT NOT NULL COMMENT '创建人id',
     updated_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
     updated_user_id BIGINT NOT NULL COMMENT '最后更新人id',
-    updated_remark VARCHAR(255) COMMENT '最后更新备注',
+    updated_remark VARCHAR(255) NOT NULL COMMENT '最后更新备注',
     deleted_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
     deleted TINYINT DEFAULT 0 NOT NULL COMMENT '是否删除，0-未删除，1-已删除'
 ) COMMENT '流水表';
