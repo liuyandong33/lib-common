323098db31ad1ffeed9484b371b9349199756033
liuyandong <554030404@qq.com>
2018-12-20 18:22:22 +0800

    促销活动修改！！

---
+++ src/main/java/build/dream/common/models/aggregatepay/ScanCodePayModel.java
@@ -0,0 +1,155 @@
+package build.dream.common.models.aggregatepay;
+
+import build.dream.common.models.BasicModel;
+
+import javax.validation.constraints.NotNull;
+
+public class ScanCodePayModel extends BasicModel {
+    @NotNull
+    private String tenantId;
+
+    @NotNull
+    private String branchId;
+
+    @NotNull
+    private Integer channelType;
+
+    @NotNull
+    private String outTradeNo;
+
+    @NotNull
+    private String authCode;
+
+    @NotNull
+    private String subject;
+
+    @NotNull
+    private Integer totalAmount;
+
+    @NotNull
+    private String notifyUrl;
+
+    @NotNull
+    private String ipAddress;
+
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public String getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(String branchId) {
+        this.branchId = branchId;
+    }
+
+    public Integer getChannelType() {
+        return channelType;
+    }
+
+    public void setChannelType(Integer channelType) {
+        this.channelType = channelType;
+    }
+
+    public String getOutTradeNo() {
+        return outTradeNo;
+    }
+
+    public void setOutTradeNo(String outTradeNo) {
+        this.outTradeNo = outTradeNo;
+    }
+
+    public String getAuthCode() {
+        return authCode;
+    }
+
+    public void setAuthCode(String authCode) {
+        this.authCode = authCode;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public Integer getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Integer totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getNotifyUrl() {
+        return notifyUrl;
+    }
+
+    public void setNotifyUrl(String notifyUrl) {
+        this.notifyUrl = notifyUrl;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public static class Builder {
+        private final ScanCodePayModel instance = new ScanCodePayModel();
+
+        public Builder tenantId(String tenantId) {
+            instance.setTenantId(tenantId);
+            return this;
+        }
+
+        public Builder branchId(String branchId) {
+            instance.setBranchId(branchId);
+            return this;
+        }
+
+        public Builder channelType(Integer channelType) {
+            instance.setChannelType(channelType);
+            return this;
+        }
+
+        public Builder outTradeNo(String outTradeNo) {
+            instance.setOutTradeNo(outTradeNo);
+            return this;
+        }
+
+        public Builder authCode(String authCode) {
+            instance.setAuthCode(authCode);
+            return this;
+        }
+
+        public Builder subject(String subject) {
+            instance.setSubject(subject);
+            return this;
+        }
+
+        public Builder totalAmount(Integer totalAmount) {
+            instance.setTotalAmount(totalAmount);
+            return this;
+        }
+
+        public Builder notifyUrl(String notifyUrl) {
+            instance.setNotifyUrl(notifyUrl);
+            return this;
+        }
+
+        public Builder ipAddress(String ipAddress) {
+            instance.setIpAddress(ipAddress);
+            return this;
+        }
+    }
+}
--- src/main/java/build/dream/common/utils/AggregatePayUtils.java
+++ src/main/java/build/dream/common/utils/AggregatePayUtils.java
@@ -1,18 +1,27 @@
 package build.dream.common.utils;
 
 import build.dream.common.constants.Constants;
+import build.dream.common.models.aggregatepay.ScanCodePayModel;
 import build.dream.common.models.alipay.AlipayTradePayModel;
 import build.dream.common.models.jingdong.FkmPayModel;
 import build.dream.common.models.weixinpay.MicroPayModel;
-import net.sf.json.JSONObject;
 import org.dom4j.DocumentException;
 
-import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Map;
 
 public class AggregatePayUtils {
-    public static Map<String, ? extends Object> scanCodePay(String tenantId, String branchId, int channelType, String outTradeNo, String authCode, String subject, int totalAmount, String notifyUrl, String ipAddress) throws IOException, DocumentException {
+    public static Map<String, ? extends Object> scanCodePay(ScanCodePayModel scanCodePayModel) throws DocumentException {
+        String tenantId = scanCodePayModel.getTenantId();
+        String branchId = scanCodePayModel.getBranchId();
+        int channelType = scanCodePayModel.getChannelType();
+        String outTradeNo = scanCodePayModel.getOutTradeNo();
+        String authCode = scanCodePayModel.getAuthCode();
+        String subject = scanCodePayModel.getSubject();
+        int totalAmount = scanCodePayModel.getTotalAmount();
+        String notifyUrl = scanCodePayModel.getNotifyUrl();
+        String ipAddress = scanCodePayModel.getIpAddress();
+
         Map<String, ? extends Object> result = null;
         if (channelType == Constants.CHANNEL_TYPE_WEI_XIN) {
             AlipayTradePayModel alipayTradePayModel = new AlipayTradePayModel();
