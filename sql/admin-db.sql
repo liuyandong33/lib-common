cb4528c1281856345ac6e1b9ec4f55c835457d1d
liuyandong <554030404@qq.com>
2018-12-26 22:16:10 +0800

    构建脚本修改！！

--- sql/admin-db.sql
+++ sql/admin-db.sql
@@ -130,3 +130,21 @@ CREATE TABLE application
     deleted_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
     deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
 ) COMMENT '应用程序';
+
+DROP TABLE IF EXISTS server;
+CREATE TABLE server
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'id',
+    host_name VARCHAR(20) NOT NULL COMMENT '主机名称',
+    ip_address VARCHAR(20) NOT NULL COMMENT 'ip 地址',
+    ssh_port INT NOT NULL COMMENT 'ssh 连接端口号',
+    user_name VARCHAR(20) NOT NULL COMMENT '用户名',
+    `password` VARCHAR(20) NOT NULL COMMENT '密码',
+    created_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
+    created_user_id BIGINT NOT NULL COMMENT '创建人id',
+    updated_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
+    updated_user_id BIGINT NOT NULL COMMENT '最后更新人id',
+    updated_remark VARCHAR(255) NOT NULL COMMENT '最后更新备注',
+    deleted_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
+    deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
+) COMMENT '服务器';
