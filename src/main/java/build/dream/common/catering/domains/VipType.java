91fc95daeb9990f444932a9949f75b98c28491ef
liuyandong <554030404@qq.com>
2019-01-01 17:08:12 +0800

    构建脚本修改！！

--- src/main/java/build/dream/common/catering/domains/VipType.java
+++ src/main/java/build/dream/common/catering/domains/VipType.java
@@ -1,5 +1,6 @@
 package build.dream.common.catering.domains;
 
+import build.dream.common.annotations.ShardingColumn;
 import build.dream.common.basic.BasicDomain;
 import build.dream.common.constants.Constants;
 
@@ -7,6 +8,7 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Date;
 
+@ShardingColumn(fieldName = VipType.FieldName.TENANT_ID, columnName = VipType.ColumnName.TENANT_ID)
 public class VipType extends BasicDomain {
     public static final String TABLE_NAME = "vip_type";
     /**
