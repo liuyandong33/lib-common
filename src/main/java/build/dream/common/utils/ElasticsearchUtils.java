93782cc19b5d2324a3a0de5331c1978785230b38
liuyandong <554030404@qq.com>
2019-01-05 20:36:20 +0800

    构建脚本修改！！

--- src/main/java/build/dream/common/utils/ElasticsearchUtils.java
+++ src/main/java/build/dream/common/utils/ElasticsearchUtils.java
@@ -2,6 +2,7 @@ package build.dream.common.utils;
 
 import build.dream.common.basic.BasicDomain;
 import build.dream.common.constants.Constants;
+import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.xcontent.XContentBuilder;
@@ -20,11 +21,11 @@ public class ElasticsearchUtils {
         return transportClient;
     }
 
-    public static IndexResponse prepareIndex(String index, String type, BasicDomain domain) throws IOException {
-        return prepareIndex(index, type, domain, Constants.DEFAULT_DATE_PATTERN);
+    public static IndexResponse index(String index, String type, BasicDomain domain) throws IOException {
+        return index(index, type, domain, Constants.DEFAULT_DATE_PATTERN);
     }
 
-    public static IndexResponse prepareIndex(String index, String type, BasicDomain domain, String datePattern) throws IOException {
+    public static IndexResponse index(String index, String type, BasicDomain domain, String datePattern) throws IOException {
         Map<String, Object> map = JacksonUtils.readValueAsMap(JacksonUtils.writeValueAsString(domain, datePattern), String.class, Object.class);
         XContentBuilder contentBuilder = XContentFactory.jsonBuilder().startObject();
         for (Map.Entry<String, Object> entry : map.entrySet()) {
@@ -33,5 +34,9 @@ public class ElasticsearchUtils {
         contentBuilder.endObject();
         IndexResponse indexResponse = obtainTransportClient().prepareIndex(index, type, domain.getId().toString()).setSource(contentBuilder).get();
         return indexResponse;
+    }
+
+    public static DeleteResponse delete(String index, String type, String id) {
+        return obtainTransportClient().prepareDelete(index, type, id).get();
     }
 }
