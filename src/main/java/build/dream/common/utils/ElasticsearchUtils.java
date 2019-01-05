553455070e612f25756c853d053fed7756f25830
liuyandong <554030404@qq.com>
2019-01-05 20:40:40 +0800

    构建脚本修改！！

--- src/main/java/build/dream/common/utils/ElasticsearchUtils.java
+++ src/main/java/build/dream/common/utils/ElasticsearchUtils.java
@@ -9,6 +9,7 @@ import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 public class ElasticsearchUtils {
@@ -27,13 +28,24 @@ public class ElasticsearchUtils {
 
     public static IndexResponse index(String index, String type, BasicDomain domain, String datePattern) throws IOException {
         Map<String, Object> map = JacksonUtils.readValueAsMap(JacksonUtils.writeValueAsString(domain, datePattern), String.class, Object.class);
+        XContentBuilder contentBuilder = buildXContentBuilder(map);
+        IndexResponse indexResponse = obtainTransportClient().prepareIndex(index, type, domain.getId().toString()).setSource(contentBuilder).get();
+        return indexResponse;
+    }
+
+    public static void indexAll(String index, String type, List<BasicDomain> domains) throws IOException {
+        for (BasicDomain domain : domains) {
+            index(index, type, domain);
+        }
+    }
+
+    public static XContentBuilder buildXContentBuilder(Map<String, Object> map) throws IOException {
         XContentBuilder contentBuilder = XContentFactory.jsonBuilder().startObject();
         for (Map.Entry<String, Object> entry : map.entrySet()) {
             contentBuilder.field(entry.getKey(), entry.getValue());
         }
         contentBuilder.endObject();
-        IndexResponse indexResponse = obtainTransportClient().prepareIndex(index, type, domain.getId().toString()).setSource(contentBuilder).get();
-        return indexResponse;
+        return contentBuilder;
     }
 
     public static DeleteResponse delete(String index, String type, String id) {
