f61aa97a3497e7f9e7862d4a67158e77b9cb6eeb
liuyandong <554030404@qq.com>
2019-02-27 16:23:15 +0800

    错误修改！！

--- src/main/java/build/dream/common/utils/ArtificialIntelligenceUtils.java
+++ src/main/java/build/dream/common/utils/ArtificialIntelligenceUtils.java
@@ -1,27 +1,37 @@
 package build.dream.common.utils;
 
+import build.dream.common.beans.WebResponse;
 import build.dream.common.constants.Constants;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.codec.digest.HmacUtils;
+import org.apache.commons.collections.MapUtils;
+import org.springframework.http.MediaType;
 
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class ArtificialIntelligenceUtils {
-    private static String generateSignature() {
-        return null;
+    private static String calculateSignature(String accessKey, String method, String accept, String body, String contentType, String date) {
+        String bodyMd5 = Base64.encodeBase64String(DigestUtils.md5(body));
+        String stringToSign = method + "\n" + accept + "\n" + bodyMd5 + "\n" + contentType + "\n" + date + "\n";
+
+        String signature = Base64.encodeBase64String(HmacUtils.hmacSha1(accessKey, stringToSign));
+        return signature;
+    }
+
+    private static String obtainGMTDate() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z", Locale.UK);
+        simpleDateFormat.setTimeZone(new SimpleTimeZone(0, Constants.GMT));
+
+        return simpleDateFormat.format(new Date());
     }
 
     public static Map<String, Object> imageTag(String accessId, String accessKey, int type, String imageUrl, String content) {
         String method = Constants.REQUEST_METHOD_POST;
-        String accept = "application/json";
-        String contentType = "application/json";
-
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z", Locale.UK);
-        simpleDateFormat.setTimeZone(new SimpleTimeZone(0, Constants.GMT));
-
-        String date = simpleDateFormat.format(new Date());
+        String accept = MediaType.APPLICATION_JSON_VALUE;
+        String contentType = MediaType.APPLICATION_JSON_VALUE;
+        String date = obtainGMTDate();
 
         Map<String, Object> requestBody = new HashMap<String, Object>();
         requestBody.put("type", type);
@@ -31,13 +41,21 @@ public class ArtificialIntelligenceUtils {
             requestBody.put("content", content);
         }
 
-        String bodyMd5 = Base64.encodeBase64String(DigestUtils.md5(GsonUtils.toJson(requestBody)));
-        String stringToSign = method + "\n" + accept + "\n" + bodyMd5 + "\n" + contentType + "\n" + date + "\n";
+        String body = GsonUtils.toJson(requestBody);
+        String signature = calculateSignature(accessKey, method, accept, body, contentType, date);
 
-        String signature = Base64.encodeBase64String(HmacUtils.hmacSha1(accessKey, stringToSign));
+        String authorization = "Dataplus " + accessId + ":" + signature;
 
-        String authHeader = "Dataplus " + accessId + ":" + signature;
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("accept", accept);
+        headers.put("content-type", contentType);
+        headers.put("date", date);
+        headers.put("Authorization", authorization);
 
-        return null;
+        WebResponse webResponse = OutUtils.doPostWithRequestBody("", headers, body);
+        Map<String, Object> resultMap = JacksonUtils.readValueAsMap(webResponse.getResult(), String.class, Object.class);
+        int errno = MapUtils.getIntValue(resultMap, "errno");
+        ValidateUtils.isTrue(errno == 0, MapUtils.getString(resultMap, "err_msg"));
+        return resultMap;
     }
 }
