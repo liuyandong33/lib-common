c717517c84c29dc8c0e4d80586bda0005e2a6248
liuyandong <554030404@qq.com>
2019-03-05 17:26:53 +0800

    支付宝接口开发！！

--- src/main/java/build/dream/common/models/alipay/AlipayOpenAgentCreateModel.java
+++ src/main/java/build/dream/common/models/alipay/AlipayOpenAgentCreateModel.java
@@ -2,6 +2,8 @@ package build.dream.common.models.alipay;
 
 import build.dream.common.models.BasicModel;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotNull;
 
@@ -13,6 +15,14 @@ public class AlipayOpenAgentCreateModel extends BasicModel {
     @NotNull
     @JsonIgnore
     private String branchId;
+
+    @NotNull
+    @JsonProperty(value = "contact_info")
+    private ContactInfo contactInfo;
+
+    @Length(max = 40)
+    @JsonProperty(value = "order_ticket")
+    private String orderTicket;
 
     public String getTenantId() {
         return tenantId;
@@ -30,6 +40,22 @@ public class AlipayOpenAgentCreateModel extends BasicModel {
         return branchId;
     }
 
+    public ContactInfo getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(ContactInfo contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public String getOrderTicket() {
+        return orderTicket;
+    }
+
+    public void setOrderTicket(String orderTicket) {
+        this.orderTicket = orderTicket;
+    }
+
     public static class Builder {
         private final AlipayOpenAgentCreateModel instance = new AlipayOpenAgentCreateModel();
 
@@ -43,15 +69,68 @@ public class AlipayOpenAgentCreateModel extends BasicModel {
             return this;
         }
 
+        public Builder contactInfo(ContactInfo contactInfo) {
+            instance.setContactInfo(contactInfo);
+            return this;
+        }
+
+        public Builder orderTicket(String orderTicket) {
+            instance.setOrderTicket(orderTicket);
+            return this;
+        }
+
         public AlipayOpenAgentCreateModel build() {
             AlipayOpenAgentCreateModel alipayOpenAgentCreateModel = new AlipayOpenAgentCreateModel();
             alipayOpenAgentCreateModel.setTenantId(instance.getTenantId());
             alipayOpenAgentCreateModel.setBranchId(instance.getBranchId());
+            alipayOpenAgentCreateModel.setContactInfo(instance.getContactInfo());
+            alipayOpenAgentCreateModel.setOrderTicket(instance.getOrderTicket());
             return alipayOpenAgentCreateModel;
         }
     }
 
     public static Builder builder() {
         return new Builder();
+    }
+
+    public static class ContactInfo extends BasicModel {
+        @NotNull
+        @Length(max = 64)
+        @JsonProperty(value = "contact_name")
+        private String contactName;
+
+        @NotNull
+        @Length(max = 16)
+        @JsonProperty(value = "contact_mobile")
+        private String contactMobile;
+
+        @NotNull
+        @Length(max = 64)
+        @JsonProperty(value = "contact_email")
+        private String contactEmail;
+
+        public String getContactName() {
+            return contactName;
+        }
+
+        public void setContactName(String contactName) {
+            this.contactName = contactName;
+        }
+
+        public String getContactMobile() {
+            return contactMobile;
+        }
+
+        public void setContactMobile(String contactMobile) {
+            this.contactMobile = contactMobile;
+        }
+
+        public String getContactEmail() {
+            return contactEmail;
+        }
+
+        public void setContactEmail(String contactEmail) {
+            this.contactEmail = contactEmail;
+        }
     }
 }
