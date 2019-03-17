0309a53f5d03ac9755059a437d25b8ab77464cee
liuyandong <554030404@qq.com>
2019-03-17 15:36:26 +0800

    支付宝接口开发！！

--- sql/catering-db.sql
+++ sql/catering-db.sql
@@ -1432,3 +1432,41 @@ CREATE TABLE menu_branch_r
     branch_id BIGINT NOT NULL COMMENT '门店ID',
     PRIMARY KEY(menu_id, tenant_id, tenant_code, branch_id)
 );
+
+DROP TABLE IF EXISTS branch_table;
+CREATE TABLE branch_table
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'id',
+    tenant_id BIGINT NOT NULL COMMENT '商户ID',
+    tenant_code VARCHAR(20) NOT NULL COMMENT '商户编码',
+    branch_id BIGINT NOT NULL COMMENT '门店ID',
+    table_area_id BIGINT NOT NULL COMMENT '桌台区域ID',
+    code VARCHAR(20) NOT NULL COMMENT '桌台编号',
+    name VARCHAR(20) NOT NULL COMMENT '桌台名称',
+    status TINYINT NOT NULL COMMENT '状态，1-启用，2-禁用',
+    dinners_number INT NOT NULL COMMENT '就餐人数',
+    created_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
+    created_user_id BIGINT NOT NULL COMMENT '创建人id',
+    updated_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
+    updated_user_id BIGINT NOT NULL COMMENT '最后更新人id',
+    updated_remark VARCHAR(255) NOT NULL COMMENT '最后更新备注',
+    deleted_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
+    deleted TINYINT DEFAULT 0 NOT NULL COMMENT '是否删除，0-未删除，1-已删除'
+) COMMENT '桌台';
+
+DROP TABLE IF EXISTS table_area;
+CREATE TABLE table_area
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'id',
+    tenant_id BIGINT NOT NULL COMMENT '商户ID',
+    tenant_code VARCHAR(20) NOT NULL COMMENT '商户编码',
+    branch_id BIGINT NOT NULL COMMENT '门店ID',
+    name VARCHAR(20) NOT NULL COMMENT '桌台区域名称',
+    created_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
+    created_user_id BIGINT NOT NULL COMMENT '创建人id',
+    updated_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
+    updated_user_id BIGINT NOT NULL COMMENT '最后更新人id',
+    updated_remark VARCHAR(255) NOT NULL COMMENT '最后更新备注',
+    deleted_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
+    deleted TINYINT DEFAULT 0 NOT NULL COMMENT '是否删除，0-未删除，1-已删除'
+) COMMENT '桌台区域';
--- src/main/java/build/dream/common/catering/domains/TableArea.java
+++ src/main/java/build/dream/common/catering/domains/TableArea.java
@@ -1,10 +1,12 @@
 package build.dream.common.catering.domains;
 
+import build.dream.common.annotations.ShardingColumn;
 import build.dream.common.basic.BasicDomain;
 
 import java.math.BigInteger;
 import java.util.Date;
 
+@ShardingColumn(fieldName = TableArea.FieldName.TENANT_ID, columnName = TableArea.ColumnName.TENANT_ID)
 public class TableArea extends BasicDomain {
     public static final String TABLE_NAME = "table_area";
     /**
