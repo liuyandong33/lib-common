a9c3ddf864e4d557de5d98bc133fcd0945625c1b
liuyandong <554030404@qq.com>
2019-03-19 21:43:58 +0800

    支付宝接口开发！！

--- sql/saas-db.sql
+++ sql/saas-db.sql
@@ -942,3 +942,25 @@ CREATE TABLE alipay_authorizer_info
     deleted_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
     deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-为删除，1-已删除'
 ) COMMENT '支付宝授权信息';
+
+DROP TABLE IF EXISTS agent_form;
+CREATE TABLE agent_form
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
+    `name` VARCHAR(20) NOT NULL COMMENT '商户名称',
+    status TINYINT NOT NULL COMMENT '状态，1-未审核，2-已审核，3-已驳回',
+    province_code VARCHAR(10) NOT NULL COMMENT '省编码',
+    province_name VARCHAR(10) NOT NULL COMMENT '省名称',
+    city_code VARCHAR(10) NOT NULL COMMENT '市编码',
+    city_name VARCHAR(10) NOT NULL COMMENT '市名称',
+    district_code VARCHAR(10) NOT NULL COMMENT '区编码',
+    district_name VARCHAR(10) NOT NULL COMMENT '区名称',
+    address VARCHAR(255) NOT NULL COMMENT '门店详细地址',
+    created_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
+    created_user_id BIGINT NOT NULL COMMENT '创建人id',
+    updated_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
+    updated_user_id BIGINT NOT NULL COMMENT '最后更新人id',
+    updated_remark VARCHAR(255) NOT NULL COMMENT '最后更新备注',
+    deleted_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
+    deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
+) COMMENT = '代理商申请单';
