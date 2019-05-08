95ca977f1a8e0d92c92645899a54f18dad7ef664
liuyandong <554030404@qq.com>
2019-05-08 12:40:29 +0800

    Redis 工具类开发！！

--- src/main/java/build/dream/common/utils/XmlUtils.java
+++ src/main/java/build/dream/common/utils/XmlUtils.java
@@ -2,7 +2,6 @@ package build.dream.common.utils;
 
 import build.dream.common.constants.Constants;
 import org.dom4j.Document;
-import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
@@ -13,15 +12,23 @@ import java.util.List;
 import java.util.Map;
 
 public class XmlUtils {
-    public static Map<String, String> xmlStringToMap(String xmlString) throws DocumentException {
-        Document document = DocumentHelper.parseText(xmlString);
-        return xmlDocumentToMap(document);
+    public static Map<String, String> xmlStringToMap(String xmlString) {
+        try {
+            Document document = DocumentHelper.parseText(xmlString);
+            return xmlDocumentToMap(document);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public static Map<String, String> xmlInputStreamToMap(InputStream inputStream) throws DocumentException {
-        SAXReader saxReader = new SAXReader();
-        Document document = saxReader.read(inputStream);
-        return xmlDocumentToMap(document);
+    public static Map<String, String> xmlInputStreamToMap(InputStream inputStream) {
+        try {
+            SAXReader saxReader = new SAXReader();
+            Document document = saxReader.read(inputStream);
+            return xmlDocumentToMap(document);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static Map<String, String> xmlDocumentToMap(Document document) {
