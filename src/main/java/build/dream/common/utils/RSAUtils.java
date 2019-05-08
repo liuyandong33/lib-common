4fee9aa0ecf940701b780ce63fb24ba4add69601
liuyandong <554030404@qq.com>
2019-05-08 12:28:35 +0800

    Redis 工具类开发！！

--- src/main/java/build/dream/common/utils/RSAUtils.java
+++ src/main/java/build/dream/common/utils/RSAUtils.java
@@ -24,14 +24,18 @@ public class RSAUtils {
     public static final String PADDING_MODE_RSA_ECB_OAEPWITHSHA1ANDMGF1PADDING = "RSA/ECB/OAEPWITHSHA-1ANDMGF1PADDING";
     public static final String PADDING_MODE_RSA_ECB_PKCS1PADDING = "RSA/ECB/PKCS1Padding";
 
-    public static Map<String, byte[]> generateKeyPair(int keySize) throws NoSuchAlgorithmException {
-        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance(KEY_ALGORITHM);
-        keyPairGen.initialize(keySize, new SecureRandom());
-        KeyPair keyPair = keyPairGen.generateKeyPair();
-        Map<String, byte[]> keyMap = new HashMap<String, byte[]>();
-        keyMap.put(Constants.PUBLIC_KEY, keyPair.getPublic().getEncoded());
-        keyMap.put(Constants.PRIVATE_KEY, keyPair.getPrivate().getEncoded());
-        return keyMap;
+    public static Map<String, byte[]> generateKeyPair(int keySize) {
+        try {
+            KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance(KEY_ALGORITHM);
+            keyPairGen.initialize(keySize, new SecureRandom());
+            KeyPair keyPair = keyPairGen.generateKeyPair();
+            Map<String, byte[]> keyMap = new HashMap<String, byte[]>();
+            keyMap.put(Constants.PUBLIC_KEY, keyPair.getPublic().getEncoded());
+            keyMap.put(Constants.PRIVATE_KEY, keyPair.getPrivate().getEncoded());
+            return keyMap;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
