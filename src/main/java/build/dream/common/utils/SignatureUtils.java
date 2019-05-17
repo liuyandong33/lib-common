3cd7c7942c1be99924704b6502b9efd0fa1dbe02
liuyandong <554030404@qq.com>
2019-05-17 08:57:10 +0800

    Redis 工具类开发！！

--- src/main/java/build/dream/common/utils/SignatureUtils.java
+++ src/main/java/build/dream/common/utils/SignatureUtils.java
@@ -1,12 +1,14 @@
 package build.dream.common.utils;
 
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
 import java.security.Signature;
 import java.security.cert.Certificate;
 
 public class SignatureUtils {
     public static final String SIGNATURE_TYPE_SHA1_WITH_RSA = "SHA1WithRSA";
     public static final String SIGNATURE_TYPE_SHA256_WITH_RSA = "SHA256withRSA";
+    public static final String SIGNATURE_TYPE_MD5_WITH_RSA = "MD5withRSA";
 
     /**
      * 获取签名器
@@ -16,8 +18,19 @@ public class SignatureUtils {
      * @throws NoSuchAlgorithmException
      */
     public static Signature obtainSignature(String signatureType) throws NoSuchAlgorithmException {
-        ValidateUtils.isTrue(SIGNATURE_TYPE_SHA1_WITH_RSA.equals(signatureType) || SIGNATURE_TYPE_SHA256_WITH_RSA.equals(signatureType), "不支持的签名方式：" + signatureType + "！");
+        ValidateUtils.isTrue(SIGNATURE_TYPE_SHA1_WITH_RSA.equals(signatureType) || SIGNATURE_TYPE_SHA256_WITH_RSA.equals(signatureType) || SIGNATURE_TYPE_MD5_WITH_RSA.equals(signatureType), "不支持的签名方式：" + signatureType + "！");
         return Signature.getInstance(signatureType);
+    }
+
+    public static byte[] sign(byte[] data, PrivateKey privateKey, String signatureType) {
+        try {
+            Signature signature = obtainSignature(signatureType);
+            signature.initSign(privateKey);
+            signature.update(data);
+            return signature.sign();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
@@ -29,14 +42,7 @@ public class SignatureUtils {
      * @return
      */
     public static byte[] sign(byte[] data, byte[] privateKey, String signatureType) {
-        try {
-            Signature signature = obtainSignature(signatureType);
-            signature.initSign(RSAUtils.restorePrivateKey(privateKey));
-            signature.update(data);
-            return signature.sign();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        return sign(data, RSAUtils.restorePrivateKey(privateKey), signatureType);
     }
 
     /**
