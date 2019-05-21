4c5df06e18a728b37d497030708af1c692692094
liuyandong <554030404@qq.com>
2019-05-21 23:02:31 +0800

    支付宝接口开发！！

---
+++ src/main/java/build/dream/common/utils/AspectUtils.java
@@ -0,0 +1,10 @@
+package build.dream.common.utils;
+
+import build.dream.common.annotations.ApiRestAction;
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class AspectUtils {
+    public static String callApiRestAction(ProceedingJoinPoint proceedingJoinPoint, ApiRestAction apiRestAction) {
+        return null;
+    }
+}
