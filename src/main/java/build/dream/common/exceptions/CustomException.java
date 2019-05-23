d535f8762b0176fd2c6d61450954a89c035e0c43
liuyandong <554030404@qq.com>
2019-05-23 09:47:01 +0800

    Redis 工具类开发！！

--- src/main/java/build/dream/common/exceptions/CustomException.java
+++ src/main/java/build/dream/common/exceptions/CustomException.java
@@ -1,12 +1,13 @@
 package build.dream.common.exceptions;
 
 import build.dream.common.constants.Constants;
+import build.dream.common.constants.ErrorCodeConstants;
 
 public class CustomException extends RuntimeException {
     private String code;
 
     public CustomException() {
-        this.code = Constants.ERROR_CODE_UNKNOWN_ERROR;
+        this("未知错误！");
     }
 
     public CustomException(String message) {
@@ -17,6 +18,16 @@ public class CustomException extends RuntimeException {
     public CustomException(String message, String code) {
         super(message);
         this.code = code;
+    }
+
+    public CustomException(String message, Throwable cause) {
+        super(message, cause);
+        this.code = ErrorCodeConstants.ERROR_CODE_UNKNOWN_ERROR;
+    }
+
+    public CustomException(Throwable cause) {
+        super(cause);
+        this.code = ErrorCodeConstants.ERROR_CODE_UNKNOWN_ERROR;
     }
 
     public CustomException(Error error) {
