7b2a0c33dab91ecc367e8fc45a8412defce57398
liuyandong <554030404@qq.com>
2019-05-24 15:51:54 +0800

    Redis 工具类开发！！

--- src/main/java/build/dream/common/classloaders/CustomClassLoader.java
+++
@@ -1,7 +0,0 @@
-package build.dream.common.classloaders;
-
-public class CustomClassLoader extends ClassLoader {
-    public Class<?> defineClassa(String name, byte[] bytes, int off, int length) throws ClassFormatError {
-        return defineClass(name, bytes, off, length);
-    }
-}
