49f3718131d76998c914b8587e5b1003a6531391
liuyandong <554030404@qq.com>
2019-05-25 19:03:55 +0800

    支付宝接口开发！！

--- src/main/java/build/dream/common/utils/PagedSearchModel.java
+++ src/main/java/build/dream/common/utils/PagedSearchModel.java
@@ -1,8 +1,6 @@
 package build.dream.common.utils;
 
 import build.dream.common.constants.Constants;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,15 +9,12 @@ import java.util.Map;
 
 public class PagedSearchModel {
     private List<SearchCondition> searchConditions = new ArrayList<SearchCondition>();
-    private List<String> columns = new ArrayList<String>();
     private String whereClause;
     private Map<String, Object> namedParameters = new HashMap<String, Object>();
-    private String tableName;
     private String groupBy;
     private String orderBy;
     private Integer page;
     private Integer rows;
-    private String databaseProvider = Constants.DATABASE_PROVIDER_MYSQL;
 
     public PagedSearchModel() {
 
@@ -45,28 +40,6 @@ public class PagedSearchModel {
         this.searchConditions = searchConditions;
     }
 
-    public String getSelectColumns() {
-        String selectColumns = null;
-        if (CollectionUtils.isEmpty(columns)) {
-            selectColumns = "*";
-        } else {
-            selectColumns = StringUtils.join(columns, ", ");
-        }
-        return selectColumns;
-    }
-
-    public List<String> getColumns() {
-        return columns;
-    }
-
-    public void setColumns(List<String> columns) {
-        this.columns = columns;
-    }
-
-    public void addColumns(String... columns) {
-        CollectionUtils.addAll(this.columns, columns);
-    }
-
     public String getWhereClause() {
         return whereClause;
     }
@@ -85,14 +58,6 @@ public class PagedSearchModel {
 
     public void addNamedParameter(String name, Object value) {
         this.namedParameters.put(name, value);
-    }
-
-    public String getTableName() {
-        return tableName;
-    }
-
-    public void setTableName(String tableName) {
-        this.tableName = tableName;
     }
 
     public void addSearchCondition(String columnName, String operationSymbol, Object searchParameter) {
@@ -129,14 +94,6 @@ public class PagedSearchModel {
 
     public void setRows(Integer rows) {
         this.rows = rows;
-    }
-
-    public String getDatabaseProvider() {
-        return databaseProvider;
-    }
-
-    public void setDatabaseProvider(String databaseProvider) {
-        this.databaseProvider = databaseProvider;
     }
 
     public Integer getOffset() {
@@ -183,16 +140,6 @@ public class PagedSearchModel {
             return this;
         }
 
-        public Builder columns(List<String> columns) {
-            instance.setColumns(columns);
-            return this;
-        }
-
-        public Builder addColumns(String... columns) {
-            CollectionUtils.addAll(instance.columns, columns);
-            return this;
-        }
-
         public Builder whereClause(String whereClause) {
             instance.setWhereClause(whereClause);
             return this;
@@ -205,11 +152,6 @@ public class PagedSearchModel {
 
         public Builder addNamedParameter(String name, Object value) {
             instance.namedParameters.put(name, value);
-            return this;
-        }
-
-        public Builder tableName(String tableName) {
-            instance.setTableName(tableName);
             return this;
         }
 
@@ -233,23 +175,15 @@ public class PagedSearchModel {
             return this;
         }
 
-        public Builder databaseProvider(String databaseProvider) {
-            instance.setDatabaseProvider(databaseProvider);
-            return this;
-        }
-
         public PagedSearchModel build() {
             PagedSearchModel pagedSearchModel = new PagedSearchModel();
             pagedSearchModel.setSearchConditions(instance.getSearchConditions());
-            pagedSearchModel.setColumns(instance.getColumns());
             pagedSearchModel.setWhereClause(instance.getWhereClause());
             pagedSearchModel.setNamedParameters(instance.getNamedParameters());
-            pagedSearchModel.setTableName(instance.getTableName());
             pagedSearchModel.setGroupBy(instance.getGroupBy());
             pagedSearchModel.setOrderBy(instance.getOrderBy());
             pagedSearchModel.setPage(instance.getPage());
             pagedSearchModel.setRows(instance.getRows());
-            pagedSearchModel.setDatabaseProvider(instance.getDatabaseProvider());
             return pagedSearchModel;
         }
     }
