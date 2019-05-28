e867ae3e24ccb35064758459a6701956d4d391f1
liuyandong <554030404@qq.com>
2019-05-28 22:56:19 +0800

    支付宝接口开发！！

--- src/main/java/build/dream/common/annotations/GenerationStrategy.java
+++ src/main/java/build/dream/common/annotations/GenerationStrategy.java
@@ -1,7 +1,18 @@
 package build.dream.common.annotations;
 
 public enum GenerationStrategy {
+    /**
+     * 自增主键
+     */
     AUTO_INCREMENT,
+
+    /**
+     * 利用生成器生成主键
+     */
     GENERATOR,
+
+    /**
+     * 利用mycat 全局序列生成主键
+     */
     MYCATSEQ_GLOBAL
 }
