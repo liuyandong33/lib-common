2125f63cdfa31de74064c7c2ab5b82ab858bb031
liuyandong <554030404@qq.com>
2019-06-17 16:38:07 +0800

    Redis 工具类开发！！

---
+++ src/main/java/build/dream/common/beans/NewLandOrgInfo.java
@@ -0,0 +1,31 @@
+package build.dream.common.beans;
+
+public class NewLandOrgInfo {
+    private String orgNo;
+    private String privateKey;
+    private String md5Key;
+
+    public String getOrgNo() {
+        return orgNo;
+    }
+
+    public void setOrgNo(String orgNo) {
+        this.orgNo = orgNo;
+    }
+
+    public String getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(String privateKey) {
+        this.privateKey = privateKey;
+    }
+
+    public String getMd5Key() {
+        return md5Key;
+    }
+
+    public void setMd5Key(String md5Key) {
+        this.md5Key = md5Key;
+    }
+}
--- src/main/java/build/dream/common/constants/Constants.java
+++ src/main/java/build/dream/common/constants/Constants.java
@@ -249,6 +249,7 @@ public class Constants {
      * @see #KEY_NEW_LAND_ACCOUNTS: 新大陆账号
      * @see #KEY_ALIPAY_AUTHORIZER_INFOS: 支付宝授权信息
      * @see #KEY_ALIPAY_DEVELOPER_ACCOUNTS: 支付宝开发者账号
+     * @see #KEY_NEW_LAND_ORG_INFOS: 新大陆机构信息
      */
     public static final String KEY_WEI_XIN_PAY_ACCOUNTS = "_wei_xin_pay_accounts";
     public static final String KEY_ALIPAY_ACCOUNTS = "_alipay_accounts";
@@ -258,6 +259,7 @@ public class Constants {
     public static final String KEY_NEW_LAND_ACCOUNTS = "_new_land_accounts";
     public static final String KEY_ALIPAY_AUTHORIZER_INFOS = "_alipay_authorizer_infos";
     public static final String KEY_ALIPAY_DEVELOPER_ACCOUNTS = "_alipay_developer_accounts";
+    public static final String KEY_NEW_LAND_ORG_INFOS = "_new_land_org_infos";
 
     // 网络错误错误信息
     public static final String NETWORK_ERROR_MESSAGE = "网络错误！";
