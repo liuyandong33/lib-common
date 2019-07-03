6c33fe2bb00931edff957bf467662ec4bf0c64fc
liuyandong <554030404@qq.com>
2019-07-03 21:26:32 +0800

    支付宝接口开发！！

--- sql/saas-db.sql
+++ sql/saas-db.sql
@@ -1047,3 +1047,22 @@ BEGIN
     RETURN current_value_today(sequence_name);
 END$$
 DELIMITER ;
+
+DROP TABLE IF EXISTS jddj_token;
+CREATE TABLE jddj_token
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'id',
+    token VARCHAR(50) NOT NULL COMMENT 'token',
+    expires_in INT NOT NULL COMMENT '有效时间',
+    `time` INT NOT NULL COMMENT '过期时间',
+    uid VARCHAR(20) NOT NULL COMMENT 'uid',
+    user_nick VARCHAR(20) NOT NULL COMMENT '用户昵称',
+    vender_id VARCHAR(20) NOT NULL COMMENT '商家ID',
+    created_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
+    created_user_id BIGINT NOT NULL COMMENT '创建人id',
+    updated_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
+    updated_user_id BIGINT NOT NULL COMMENT '最后更新人id',
+    updated_remark VARCHAR(255) NOT NULL COMMENT '最后更新备注',
+    deleted_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
+    deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
+) COMMENT '京东到家token';
