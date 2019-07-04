f998661040af73b25be9ad0d1731a9948f8d6f06
liuyandong <554030404@qq.com>
2019-07-04 16:59:46 +0800

    Redis 工具类开发！！

--- sql/saas-db.sql
+++ sql/saas-db.sql
@@ -1069,3 +1069,18 @@ CREATE TABLE jddj_token
     deleted_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
     deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
 ) COMMENT '京东到家token';
+
+DROP TABLE IF EXISTS jddj_code;
+CREATE TABLE jddj_code
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'id',
+    `code` VARCHAR(50) NOT NULL COMMENT 'code',
+    vender_id VARCHAR(20) NOT NULL COMMENT '商家ID',
+    created_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
+    created_user_id BIGINT NOT NULL COMMENT '创建人id',
+    updated_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
+    updated_user_id BIGINT NOT NULL COMMENT '最后更新人id',
+    updated_remark VARCHAR(255) NOT NULL COMMENT '最后更新备注',
+    deleted_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
+    deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
+) COMMENT '京东到家code';
