ee82de5e88095ccf2c33583789ffab52cf039522
liuyandong <554030404@qq.com>
2019-07-12 13:42:55 +0800

    Redis 工具类开发！！

--- src/main/java/build/dream/common/utils/SmsUtils.java
+++ src/main/java/build/dream/common/utils/SmsUtils.java
@@ -13,7 +13,7 @@ import java.util.*;
 public class SmsUtils {
     private static final String ACCESS_KEY_ID = ConfigurationUtils.getConfiguration(Constants.ALIYUN_ACCESS_KEY_ID);
     private static final String ACCESS_SECRET = ConfigurationUtils.getConfiguration(Constants.ALIYUN_ACCESS_KEY_SECRET);
-    private static final String DY_SMS_API_URL = "http://dysmsapi.aliyuncs.com";
+    private static final String DY_SMS_API_URL = "https://dysmsapi.aliyuncs.com";
 
     public static Map<String, Object> sendSms(SendSmsModel sendSmsModel) {
         String phoneNumbers = sendSmsModel.getPhoneNumbers();
@@ -57,8 +57,15 @@ public class SmsUtils {
         Map<String, String> sortedRequestParameters = new TreeMap<String, String>(requestParameters);
         List<String> requestParameterPairs = new ArrayList<String>();
         for (Map.Entry<String, String> entry : sortedRequestParameters.entrySet()) {
-            requestParameterPairs.add(entry.getKey() + "=" + UrlUtils.encode(entry.getValue(), Constants.CHARSET_NAME_UTF_8));
+            requestParameterPairs.add(UrlUtils.encode(entry.getKey(), Constants.CHARSET_NAME_UTF_8) + "=" + UrlUtils.encode(entry.getValue(), Constants.CHARSET_NAME_UTF_8));
         }
-        return Base64.encodeBase64String(HmacUtils.hmacSha1(accessSecret, StringUtils.join(requestParameterPairs, "&")));
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(Constants.REQUEST_METHOD_POST);
+        stringBuilder.append("&");
+        stringBuilder.append(UrlUtils.encode("/", Constants.CHARSET_NAME_UTF_8));
+        stringBuilder.append("&");
+        stringBuilder.append(UrlUtils.encode(StringUtils.join(requestParameterPairs, "&"), Constants.CHARSET_NAME_UTF_8));
+        return Base64.encodeBase64String(HmacUtils.hmacSha1(accessSecret + "&", stringBuilder.toString()));
     }
 }
