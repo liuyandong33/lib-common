2f03c288400a314642dfb5292ecf7da872a6115d
liuyandong <554030404@qq.com>
2019-07-30 16:08:49 +0800

    Redis 工具类开发！！

--- src/main/java/build/dream/common/utils/ChineseUtils.java
+++ src/main/java/build/dream/common/utils/ChineseUtils.java
@@ -3,8 +3,8 @@ package build.dream.common.utils;
 import net.sourceforge.pinyin4j.PinyinHelper;
 import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
 import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
-import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
 import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
 
 public class ChineseUtils {
     private static HanyuPinyinOutputFormat hanyuPinyinOutputFormat;
@@ -14,27 +14,53 @@ public class ChineseUtils {
         hanyuPinyinOutputFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
     }
 
-    public static String[] obtainPinyinStringArray(char chinese) throws BadHanyuPinyinOutputFormatCombination {
-        return PinyinHelper.toHanyuPinyinStringArray(chinese, hanyuPinyinOutputFormat);
+    public static String[] obtainPinyinStringArray(char chinese) {
+        return ApplicationHandler.callMethodSuppressThrow(() -> PinyinHelper.toHanyuPinyinStringArray(chinese, hanyuPinyinOutputFormat));
     }
 
     public static boolean isChinese(char c) {
         return c >= 0x4E00 && c <= 0x9FA5;
     }
 
-    public static String obtainPinyinFirstLetter(String str) throws BadHanyuPinyinOutputFormatCombination {
-        str = str.replace(" ", "");
+    public static boolean isNotChinese(char c) {
+        return !isChinese(c);
+    }
+
+    public static String obtainPinyinFirstLetter(String chinese) {
+        String str = StringUtils.deleteWhitespace(chinese);
         char[] charArray = str.toCharArray();
         StringBuilder stringBuilder = new StringBuilder();
         for (char c : charArray) {
-            if (isChinese(c)) {
-                String[] pinyinStringArray = obtainPinyinStringArray(c);
-                if (ArrayUtils.isNotEmpty(pinyinStringArray)) {
-                    stringBuilder.append(pinyinStringArray[0].charAt(0));
-                }
-            } else {
+            if (isNotChinese(c)) {
                 stringBuilder.append(c);
+                continue;
             }
+
+            String[] pinyinStringArray = obtainPinyinStringArray(c);
+            if (ArrayUtils.isEmpty(pinyinStringArray)) {
+                continue;
+            }
+
+            stringBuilder.append(pinyinStringArray[0].charAt(0));
+        }
+        return stringBuilder.toString();
+    }
+
+    public static String obtainPinyin(String chinese) {
+        String str = StringUtils.deleteWhitespace(chinese);
+        char[] charArray = str.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char c : charArray) {
+            if (isNotChinese(c)) {
+                stringBuilder.append(c);
+                continue;
+            }
+            String[] pinyinStringArray = obtainPinyinStringArray(c);
+            if (ArrayUtils.isEmpty(pinyinStringArray)) {
+                continue;
+            }
+
+            stringBuilder.append(pinyinStringArray[0]);
         }
         return stringBuilder.toString();
     }
