d584e3fb2d3b1d64cb6fe187d0837925a3a81747
liuyandong <554030404@qq.com>
2019-08-05 17:23:06 +0800

    Redis 工具类开发！！

--- src/main/java/build/dream/common/models/push/MessageModel.java
+++ src/main/java/build/dream/common/models/push/MessageModel.java
@@ -5,6 +5,22 @@ import build.dream.common.models.BasicModel;
 import javax.validation.constraints.NotNull;
 
 public class MessageModel extends BasicModel {
+    /**
+     * 消息类型
+     * 1-饿了么订单生效
+     * 2-订单被取消（接单前）
+     * 3-下单用户申请取消
+     * 4-用户取消取消订单
+     * 5-用户申请仲裁
+     * 6-客服仲裁取消单申请有效
+     * 7-客服仲裁取消单申请无效
+     * 8-用户申请退单
+     * 9-用户取消退单
+     * 10-用户申请仲裁
+     * 11-客服仲裁退单有效
+     * 12-客服仲裁退单无效
+     * 13-用户催单
+     */
     @NotNull
     private Integer type;
 
