d13d8657b96d8e44c887fbad2569f57d269f4722
liuyandong <554030404@qq.com>
2019-08-06 22:25:35 +0800

    支付宝接口开发！！

--- src/main/java/build/dream/common/iot/domains/Device.java
+++ src/main/java/build/dream/common/iot/domains/Device.java
@@ -3,11 +3,69 @@ package build.dream.common.iot.domains;
 import build.dream.common.basic.BasicDomain;
 import build.dream.common.saas.domains.Tenant;
 
+import java.math.BigInteger;
+
 public class Device extends BasicDomain {
+    public static final String TABLE_NAME = "pos";
+    /**
+     * 商户ID
+     */
+    private BigInteger tenantId;
+    /**
+     * 商户编号
+     */
+    private String tenantCode;
+    /**
+     * 门店ID
+     */
+    private BigInteger branchId;
+
+    public BigInteger getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(BigInteger tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public String getTenantCode() {
+        return tenantCode;
+    }
+
+    public void setTenantCode(String tenantCode) {
+        this.tenantCode = tenantCode;
+    }
+
+    public BigInteger getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(BigInteger branchId) {
+        this.branchId = branchId;
+    }
+
     public static class Builder extends BasicDomain.Builder<Builder, Device> {
+        public Builder tenantId(BigInteger tenantId) {
+            instance.setTenantId(tenantId);
+            return this;
+        }
+
+        public Builder tenantCode(String tenantCode) {
+            instance.setTenantCode(tenantCode);
+            return this;
+        }
+
+        public Builder branchId(BigInteger branchId) {
+            instance.setBranchId(branchId);
+            return this;
+        }
+
         @Override
         public Device build() {
             Device device = super.build();
+            device.setTenantId(instance.getTenantId());
+            device.setTenantCode(instance.getTenantCode());
+            device.setBranchId(instance.getBranchId());
             return device;
         }
     }
@@ -17,8 +75,14 @@ public class Device extends BasicDomain {
     }
 
     public static final class ColumnName extends BasicDomain.ColumnName {
+        public static final String TENANT_ID = "tenant_id";
+        public static final String TENANT_CODE = "tenant_code";
+        public static final String BRANCH_ID = "branch_id";
     }
 
     public static final class FieldName extends BasicDomain.FieldName {
+        public static final String TENANT_ID = "tenantId";
+        public static final String TENANT_CODE = "tenantCode";
+        public static final String BRANCH_ID = "branchId";
     }
 }
