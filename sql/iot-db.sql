3f025c21dd0d6a15cf1db0cd18261a41f7ca13cb
liuyandong <554030404@qq.com>
2019-08-06 22:44:33 +0800

    支付宝接口开发！！

--- sql/iot-db.sql
+++ sql/iot-db.sql
@@ -2,6 +2,9 @@ DROP TABLE IF EXISTS device;
 CREATE TABLE device
 (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
+    tenant_id BIGINT NOT NULL COMMENT '商户ID',
+    tenant_code VARCHAR(20) NOT NULL COMMENT '商户编号',
+    branch_id BIGINT NOT NULL COMMENT '门店ID',
     created_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
     created_user_id BIGINT NOT NULL COMMENT '创建人id',
     updated_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
@@ -9,4 +12,4 @@ CREATE TABLE device
     updated_remark VARCHAR(255) NOT NULL COMMENT '最后更新备注',
     deleted_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
     deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
-);
+) COMMENT '设备';
