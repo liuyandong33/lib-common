15263185f65ea87fa49a28897b908dde081ea153
liuyandong <554030404@qq.com>
2019-08-07 16:00:52 +0800

    Redis 工具类开发！！

--- src/main/java/build/dream/common/iot/domains/Device.java
+++ src/main/java/build/dream/common/iot/domains/Device.java
@@ -19,6 +19,21 @@ public class Device extends BasicDomain {
      */
     private BigInteger branchId;
 
+    /**
+     * 设备名称
+     */
+    private String name;
+
+    /**
+     * 设备编号
+     */
+    private String code;
+
+    /**
+     * 设备类型，1-温控设备
+     */
+    private Integer type;
+
     public BigInteger getTenantId() {
         return tenantId;
     }
@@ -43,6 +58,30 @@ public class Device extends BasicDomain {
         this.branchId = branchId;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
     public static class Builder extends BasicDomain.Builder<Builder, Device> {
         public Builder tenantId(BigInteger tenantId) {
             instance.setTenantId(tenantId);
@@ -59,12 +98,30 @@ public class Device extends BasicDomain {
             return this;
         }
 
+        public Builder name(String name) {
+            instance.setName(name);
+            return this;
+        }
+
+        public Builder code(String code) {
+            instance.setCode(code);
+            return this;
+        }
+
+        public Builder type(Integer type) {
+            instance.setType(type);
+            return this;
+        }
+
         @Override
         public Device build() {
             Device device = super.build();
             device.setTenantId(instance.getTenantId());
             device.setTenantCode(instance.getTenantCode());
             device.setBranchId(instance.getBranchId());
+            device.setName(instance.getName());
+            device.setCode(instance.getCode());
+            device.setType(instance.getType());
             return device;
         }
     }
@@ -77,11 +134,17 @@ public class Device extends BasicDomain {
         public static final String TENANT_ID = "tenant_id";
         public static final String TENANT_CODE = "tenant_code";
         public static final String BRANCH_ID = "branch_id";
+        public static final String NAME = "name";
+        public static final String CODE = "code";
+        public static final String TYPE = "type";
     }
 
     public static final class FieldName extends BasicDomain.FieldName {
         public static final String TENANT_ID = "tenantId";
         public static final String TENANT_CODE = "tenantCode";
         public static final String BRANCH_ID = "branchId";
+        public static final String NAME = "name";
+        public static final String CODE = "code";
+        public static final String TYPE = "type";
     }
 }
