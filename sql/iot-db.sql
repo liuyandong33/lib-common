5cc01cc4b21f09f69c0dd77dcf23cd17f197305f
liuyandong <554030404@qq.com>
2019-08-07 10:41:02 +0800

    Redis 工具类开发！！

--- sql/iot-db.sql
+++ sql/iot-db.sql
@@ -1,3 +1,50 @@
+DROP TABLE IF EXISTS merge_user_branch;
+CREATE TABLE merge_user_branch
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
+    user_id BIGINT NOT NULL COMMENT '用户ID',
+    tenant_id BIGINT NOT NULL COMMENT '商户ID',
+    tenant_code VARCHAR(20) NOT NULL COMMENT '商户编码',
+    branch_id BIGINT NOT NULL COMMENT '门店ID',
+    created_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
+    created_user_id BIGINT NOT NULL COMMENT '创建人id',
+    updated_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
+    updated_user_id BIGINT NOT NULL COMMENT '最后更新人id',
+    updated_remark VARCHAR(255) NOT NULL COMMENT '最后更新备注',
+    deleted_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 0 时有意义，默认值为1970-01-01 00:00:00',
+    deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
+) COMMENT '用户与门店关联表';
+
+DROP TABLE IF EXISTS branch;
+CREATE TABLE branch
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
+    tenant_id BIGINT NOT NULL COMMENT '商户ID',
+    tenant_code VARCHAR(20) NOT NULL COMMENT '商户编码',
+    `code` VARCHAR(20) NOT NULL COMMENT '门店编码',
+    `name` VARCHAR(20) NOT NULL COMMENT '门店名称',
+    `type` TINYINT NOT NULL COMMENT '1-总部，2-直营店，3-加盟店，4-配送中心',
+    `status` TINYINT NOT NULL COMMENT '状态，1-启用，2-停用',
+    province_code VARCHAR(10) NOT NULL COMMENT '省编码',
+    province_name VARCHAR(10) NOT NULL COMMENT '省名称',
+    city_code VARCHAR(10) NOT NULL COMMENT '市编码',
+    city_name VARCHAR(10) NOT NULL COMMENT '市名称',
+    district_code VARCHAR(10) NOT NULL COMMENT '区编码',
+    district_name VARCHAR(10) NOT NULL COMMENT '区名称',
+    address VARCHAR(255) NOT NULL COMMENT '门店详细地址',
+    longitude VARCHAR(20) NOT NULL COMMENT '经度',
+    latitude VARCHAR(20) NOT NULL COMMENT '纬度',
+    linkman VARCHAR(20) NOT NULL COMMENT '联系人',
+    contact_phone VARCHAR(20) NOT NULL COMMENT '联系电话',
+    created_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
+    created_user_id BIGINT NOT NULL COMMENT '创建人id',
+    updated_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
+    updated_user_id BIGINT NOT NULL COMMENT '最后更新人id',
+    updated_remark VARCHAR(255) NOT NULL COMMENT '最后更新备注',
+    deleted_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
+    deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
+) COMMENT '门店表';
+
 DROP TABLE IF EXISTS device;
 CREATE TABLE device
 (
