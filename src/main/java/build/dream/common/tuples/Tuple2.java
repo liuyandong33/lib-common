002375ce886ab1f8188098a197cf2360b7401a32
liuyandong <554030404@qq.com>
2019-08-08 19:52:33 +0800

    支付宝接口开发！！

--- src/main/java/build/dream/common/tuples/Tuple2.java
+++ src/main/java/build/dream/common/tuples/Tuple2.java
@@ -1,17 +1,14 @@
 package build.dream.common.tuples;
 
-public class Tuple2<T1, T2> {
-    private T1 _1;
+public class Tuple2<T1, T2> extends Tuple1<T1> {
     private T2 _2;
 
     public Tuple2(T1 _1, T2 _2) {
-        this._1 = _1;
+        super(_1);
         this._2 = _2;
     }
 
-    public T1 _1() {
-        return _1;
+    public T2 _2() {
+        return _2;
     }
-
-    public T2 _2() {return _2;}
 }
--- src/main/java/build/dream/common/tuples/Tuple3.java
+++ src/main/java/build/dream/common/tuples/Tuple3.java
@@ -1,22 +1,11 @@
 package build.dream.common.tuples;
 
-public class Tuple3<T1, T2, T3> {
-    private T1 _1;
-    private T2 _2;
+public class Tuple3<T1, T2, T3> extends Tuple2<T1, T2> {
     private T3 _3;
 
     public Tuple3(T1 _1, T2 _2, T3 _3) {
-        this._1 = _1;
-        this._2 = _2;
+        super(_1, _2);
         this._3 = _3;
-    }
-
-    public T1 _1() {
-        return _1;
-    }
-
-    public T2 _2() {
-        return _2;
     }
 
     public T3 _3() {
