09fc83b9179cfaf7f106a21447192e96e48bb42f
liuyandong <554030404@qq.com>
2019-08-09 11:23:30 +0800

    Redis 工具类开发！！

--- src/main/java/build/dream/common/tuples/Tuple3.java
+++ src/main/java/build/dream/common/tuples/Tuple3.java
@@ -3,6 +3,9 @@ package build.dream.common.tuples;
 public class Tuple3<T1, T2, T3> extends Tuple2<T1, T2> {
     private T3 _3;
 
+    public Tuple3() {
+    }
+
     public Tuple3(T1 _1, T2 _2, T3 _3) {
         super(_1, _2);
         this._3 = _3;
--- src/main/java/build/dream/common/tuples/Tuple6.java
+++ src/main/java/build/dream/common/tuples/Tuple6.java
@@ -3,6 +3,9 @@ package build.dream.common.tuples;
 public class Tuple6<T1, T2, T3, T4, T5, T6> extends Tuple5<T1, T2, T3, T4, T5> {
     private T6 _6;
 
+    public Tuple6() {
+    }
+
     public Tuple6(T1 _1, T2 _2, T3 _3, T4 _4, T5 _5, T6 _6) {
         super(_1, _2, _3, _4, _5);
         this._6 = _6;
