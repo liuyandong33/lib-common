35edb0fbd70a09c6011c60f160ac7ae9ddbc7cd6
liuyandong <554030404@qq.com>
2019-08-30 08:55:37 +0800

    错误修改！！

--- src/main/java/build/dream/common/utils/ThreadUtils.java
+++ src/main/java/build/dream/common/utils/ThreadUtils.java
@@ -1,7 +1,13 @@
 package build.dream.common.utils;
 
+import java.util.concurrent.TimeUnit;
+
 public class ThreadUtils {
     public static void sleepSafe(long millis) {
         ApplicationHandler.callMethodSuppressThrow(() -> Thread.sleep(millis));
+    }
+
+    public static void sleepSafe(TimeUnit timeUnit, long timeout) {
+        ApplicationHandler.callMethodSuppressThrow(() -> timeUnit.sleep(timeout));
     }
 }
