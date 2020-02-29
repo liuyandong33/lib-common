c4b89f71b07dee45efc972f3d3cbd00e8a22500b
liuyandong <554030404@qq.com>
2020-02-29 18:36:24 +0800

    支付宝接口开发！！

--- src/main/java/build/dream/common/models/rocketmq/DelayedOrTimedType.java
+++ src/main/java/build/dream/common/models/rocketmq/DelayedOrTimedType.java
@@ -1,6 +1,6 @@
 package build.dream.common.models.rocketmq;
 
 public enum DelayedOrTimedType {
-    DELAYED_OR_TIMED_TYPE_DIET_ORDER_INVALID,
-    DELAYED_OR_TIMED_TYPE_POS_MQTT_TOKEN_INVALID;
+    DELAYED_TYPE_DIET_ORDER_INVALID,
+    DELAYED_TYPE_POS_MQTT_TOKEN_INVALID;
 }
