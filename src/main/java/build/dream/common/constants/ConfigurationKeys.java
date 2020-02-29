307794c3d35060f493f821ebe15de4c63d6fe5f5
liuyandong <554030404@qq.com>
2020-02-29 23:47:55 +0800

    支付宝接口开发！！

---
+++ src/main/java/build/dream/common/constants/ConfigurationKeys.java
@@ -0,0 +1,33 @@
+package build.dream.common.constants;
+
+public class ConfigurationKeys {
+    /**
+     * 部署环境
+     */
+    public static final String DEPLOYMENT_ENVIRONMENT = "deployment.environment";
+
+    /**
+     * 分区码
+     */
+    public static final String PARTITION_CODE = "partition.code";
+
+    /**
+     * 服务名称
+     */
+    public static final String SERVICE_NAME = "service.name";
+
+    /**
+     * 服务端口
+     */
+    public static final String SERVER_PORT = "server.port";
+
+    /**
+     * 注册中心中服务名称
+     */
+    public static final String SPRING_APPLICATION_NAME = "spring.application.name";
+
+    /**
+     * 主域名
+     */
+    public static final String HOME_URL = "home.url";
+}
