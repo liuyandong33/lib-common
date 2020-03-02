6157f747acc8f318bb1102773ca31d2d1bf27bfa
liuyandong <554030404@qq.com>
2020-03-02 19:29:33 +0800

    支付宝接口开发！！

--- src/main/java/build/dream/common/utils/OauthClientDetailUtils.java
+++ src/main/java/build/dream/common/utils/OauthClientDetailUtils.java
@@ -1,6 +1,5 @@
 package build.dream.common.utils;
 
-import build.dream.common.constants.Constants;
 import build.dream.common.constants.RedisKeys;
 import build.dream.common.domains.saas.OauthClientDetail;
 import org.apache.commons.collections.MapUtils;
@@ -15,12 +14,12 @@ public class OauthClientDetailUtils {
         Map<String, String> oauthClientDetailMap = oauthClientDetails.stream().collect(Collectors.toMap(oauthClientDetail -> oauthClientDetail.getClientId(), oauthClientDetail -> JacksonUtils.writeValueAsString(oauthClientDetail)));
         CommonRedisUtils.del(RedisKeys.KEY_OAUTH_CLIENT_DETAILS);
         if (MapUtils.isNotEmpty(oauthClientDetailMap)) {
-            CommonRedisUtils.hmset(Constants.KEY_USER_INFOS, oauthClientDetailMap);
+            CommonRedisUtils.hmset(RedisKeys.KEY_OAUTH_CLIENT_DETAILS, oauthClientDetailMap);
         }
     }
 
     public static OauthClientDetail obtainOauthClientDetail(String clientId) {
-        String oauthClientDetailJson = CommonRedisUtils.get(clientId);
+        String oauthClientDetailJson = CommonRedisUtils.hget(RedisKeys.KEY_OAUTH_CLIENT_DETAILS, clientId);
         if (StringUtils.isNotBlank(oauthClientDetailJson)) {
             return JacksonUtils.readValue(oauthClientDetailJson, OauthClientDetail.class);
         }
