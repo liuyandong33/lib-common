730780b8f8d9abab549c9a549313d03bf4ab507d
liuyandong <554030404@qq.com>
2020-03-03 17:16:37 +0800

    支付宝接口开发！！

--- sql/admin-db.sql
+++ sql/admin-db.sql
@@ -149,8 +149,8 @@ CREATE TABLE server
     deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
 ) COMMENT '服务器';
 
-DROP TABLE IF EXISTS jvm_operation;
-CREATE TABLE jvm_operation
+DROP TABLE IF EXISTS java_operation;
+CREATE TABLE java_operation
 (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
     xms VARCHAR(20) NOT NULL COMMENT 'JVM启动时申请的初始Heap值，默认为操作系统物理内存的1/64但小于1G。默认当空余堆内存大于70%时，JVM会减小heap的大小到-Xms指定的大小，可通过-XX:MaxHeapFreeRation=来指定这个比列。Server端JVM最好将-Xms和-Xmx设为相同值，避免每次垃圾回收完成后JVM重新分配内存；开发测试机JVM可以保留默认值。(例如：-Xms4g)',
