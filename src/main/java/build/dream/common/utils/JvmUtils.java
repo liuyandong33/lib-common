b3110f65a2476c4dcb7de4f13311a42ef443ad8b
liuyandong <554030404@qq.com>
2020-03-17 21:43:14 +0800

    支付宝接口开发！！

--- src/main/java/build/dream/common/utils/JvmUtils.java
+++ src/main/java/build/dream/common/utils/JvmUtils.java
@@ -1,37 +1,23 @@
 package build.dream.common.utils;
 
-import org.springframework.util.ReflectionUtils;
-import sun.management.VMManagement;
-
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
 import java.lang.management.RuntimeMXBean;
-import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
 public class JvmUtils {
     private static MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
     private static RuntimeMXBean runtimeMXBean = ManagementFactory.getRuntimeMXBean();
-    private static VMManagement vmManagement;
-
-    static {
-        Field field = ReflectionUtils.findField(memoryMXBean.getClass(), "jvm");
-        ReflectionUtils.makeAccessible(field);
-        vmManagement = (VMManagement) ReflectionUtils.getField(field, memoryMXBean);
-    }
-
-    public static List<String> getVmArguments() {
-        return vmManagement.getVmArguments();
-    }
-
-    public static String getVmId() {
-        return vmManagement.getVmId();
-    }
 
     public static int getProcessId() {
-        String vmId = vmManagement.getVmId();
-        return Integer.parseInt(vmId.substring(0, vmId.indexOf("@")));
+        String name = getName();
+        return Integer.parseInt(name.substring(0, name.indexOf("@")));
+    }
+
+    public static String getName() {
+        return runtimeMXBean.getName();
     }
 
     public static String getVmName() {
@@ -92,5 +78,25 @@ public class JvmUtils {
 
     public static Map<String, String> getSystemProperties() {
         return runtimeMXBean.getSystemProperties();
+    }
+
+    public static MemoryUsage getHeapMemoryUsage() {
+        return memoryMXBean.getHeapMemoryUsage();
+    }
+
+    public static MemoryUsage getNonHeapMemoryUsage() {
+        return memoryMXBean.getNonHeapMemoryUsage();
+    }
+
+    public static boolean isVerbose() {
+        return memoryMXBean.isVerbose();
+    }
+
+    public static void setVerbose(boolean value) {
+        memoryMXBean.setVerbose(value);
+    }
+
+    public static void gc() {
+        memoryMXBean.gc();
     }
 }
